672c105f4260ceaef5a2a2d70cf67fbf1687d4c4
Yan Wang <wayan@nvidia.com>
2024-02-13 16:43:03 +0100

    Compute memory (PR2076)

---
+++ thunder/examine/memory_caculation.py
@@ -0,0 +1,156 @@
+from collections.abc import Callable
+
+from thunder.core.prims import PrimIDs
+from thunder.core.proxies import FutureTensorProxy, Proxy, TensorProxy
+from thunder.core.symbol import BoundSymbol, Symbol
+from thunder.core.trace import TraceCtx
+from thunder.core.utils import check_type, ProxyDict
+
+memory_calculate_skip_list = (PrimIDs.RETURN, "clear_collection")
+
+# List of operators that considered no memory changes occurred in Thunder.
+# NOTE: for the operators have different input and output shape, such as expand,
+# the final del statement always reduce the memory size by the initial size of the input tensor
+thunder_alias_operator_list = (
+    "unsqueeze",
+    "squeeze",
+    "transpose",
+    "expand",
+    "reshape",
+    "permute",
+    "contiguous",
+    "torch_wait_prim_impl",
+)
+
+# A whitelist registry of symbols that require special memory calculation;
+# if not registered, the default memory calculation function is used.
+memory_calculate_impls: dict[Symbol, Callable] = dict()
+
+
+def register_memory_calculate_function(*ops):
+    """Decorator to register memory calculation function for a list of symbol."""
+
+    def decorator(func):
+        for op in ops:
+            memory_calculate_impls[op] = func
+        return func
+
+    return decorator
+
+
+class MemoryData:
+    """
+    Helper class to track the reference count of a Proxy
+    """
+
+    def __init__(self, n: int, proxy: Proxy):
+        self._cnt = n
+        self._proxy = proxy
+
+    def incr_ref(self):
+        self._cnt += 1
+        return self._cnt
+
+    def decr_ref(self):
+        if self._cnt == 0:
+            raise ValueError(f"Refernce count of {self._proxy} cannot be negative")
+        self._cnt -= 1
+        return self._cnt
+
+    def get_memory_size(self):
+        check_type(self._proxy, Proxy)
+        return self._proxy.numel * self._proxy.dtype.bytes
+
+
+def default_alloc_memory(
+    bsym: BoundSymbol, tensor_to_memory_data: ProxyDict, name_to_alloc_memory: dict[str, int]
+) -> int:
+    """
+    The default function to calculate the memory usage, covers the most common situation where memory is increased by the size of the output tensors.
+
+    Args:
+        bsym (BoundSymbol): The bound symbol that needs to calculate the memory usage
+        tensor_to_memory_data (ProxyDict): A dict to record the reference count of proxy
+        name_to_alloc_memory (dict): A dict to record the bound symbol's name and the proxy names as key and the size of memory change as value
+
+    Returns:
+        int: The size of memory change caused by the input bsym
+    """
+    # Skip CollectionProxy(output of input unpacking operators, such as unpack_sequence)
+    # and other negligible scalar types
+    tensor_outs = [x for x in bsym.flat_proxy_outs if isinstance(x, (TensorProxy, FutureTensorProxy))]
+    result = sum(t.numel * t.dtype.bytes for t in tensor_outs)
+    for x in tensor_outs:
+        assert x not in tensor_to_memory_data
+        tensor_to_memory_data[x] = MemoryData(n=1, proxy=x)
+
+    name_to_alloc_memory[f"{bsym.sym.name} {', '.join(t.name for t in tensor_outs)}"] = result
+    return result
+
+
+# Thunder always assigns different tensor names for the input and output of a bound symbol,
+# even if there are situations where some tensor names refer to the same memory.
+# the reference count and original proxy of the aliases are tracked in tensor_to_memory_data
+@register_memory_calculate_function(*thunder_alias_operator_list)
+def track_alias_op_memory(
+    bsym: BoundSymbol, tensor_to_memory_data: ProxyDict, name_to_alloc_memory: dict[str, int]
+) -> int:
+    inp = bsym.flat_proxy_args[0]
+    out = bsym.flat_proxy_outs[0]
+    assert inp in tensor_to_memory_data
+    tensor_to_memory_data[inp].incr_ref()
+    tensor_to_memory_data[out] = tensor_to_memory_data[inp]
+    return 0
+
+
+@register_memory_calculate_function(PrimIDs.DEL)
+def del_op_memory(bsym: BoundSymbol, tensor_to_memory_data: ProxyDict, name_to_alloc_memory: dict[str, int]) -> int:
+    tensor_args = (x for x in bsym.flat_proxy_args if isinstance(x, (TensorProxy, FutureTensorProxy)))
+    memory_size = 0
+    for a in tensor_args:
+        assert a in tensor_to_memory_data
+        cnt_a = tensor_to_memory_data[a].decr_ref()
+
+        if cnt_a == 0:
+            size_a = tensor_to_memory_data[a].get_memory_size()
+            memory_size -= size_a
+            name_to_alloc_memory[f"{bsym.sym.name} {a.name}"] = -size_a
+    return memory_size
+
+
+def get_alloc_memory(trc: TraceCtx) -> tuple[int, dict[str, int]]:
+    """
+    Calculate the memory usage based on the executable trace.
+    The memory calculation is based only on the compile-time trace, i.e. the input and output shape
+    and type of the operator, without taking into account any kind of caching e.g. CudaCachedAllocator,
+    or any allocation for the intermediate within the operator (e.g.: nvFusion ops).
+    Note that the trace doesn't contain strides. Only the operators in `thunder_alias_operator_list` are assumed to
+    cause no memory changes, this is different from the actual memory allocation for Torch view operators, such as reshape.
+    So the estimated memory usage may differ from the runtime memory usage.
+
+
+    Args:
+        trc (TraceCtx): The executable Thunder trace.
+
+    Returns:
+        tuple[int, dict[str, int]]: The peak memory usage and a dict mapping the symbol name and
+        associated tensor names to the number of bytes of memory.
+    """
+    name_to_alloc_memory: dict[str, int] = dict()
+    max_allocated = 0
+    allocated = 0
+
+    tensor_to_memory_data = ProxyDict()
+    for bsym in trc.bound_symbols:
+        if bsym.sym.id in memory_calculate_skip_list:
+            continue
+        impl = memory_calculate_impls.get(bsym.sym.id)
+        if impl is None:
+            res = default_alloc_memory(bsym, tensor_to_memory_data, name_to_alloc_memory)
+            allocated += res
+        else:
+            allocated += impl(bsym, tensor_to_memory_data, name_to_alloc_memory)
+
+        max_allocated = max(max_allocated, allocated)
+
+    return max_allocated, name_to_alloc_memory
---
+++ thunder/tests/test_examine_memory.py
@@ -0,0 +1,163 @@
+from contextlib import contextmanager
+from functools import partial
+
+import pytest
+
+import torch
+
+import thunder
+import thunder.core.devices as devices
+import thunder.core.dtypes as dtypes
+import thunder.torch as ltorch
+from thunder.core.proxies import TensorProxy
+from thunder.examine.memory_caculation import get_alloc_memory
+
+from thunder.tests.framework import instantiate, nvFuserTestExecutor, TorchTestExecutor
+from thunder.tests.make_tensor import make_tensor
+
+
+@contextmanager
+def runtime_allocated_memory(dev):
+    torch.cuda.reset_peak_memory_stats(dev)
+    try:
+        yield
+    finally:
+        memory_states = torch.cuda.memory_stats(dev)
+        alloc = memory_states["allocated_bytes.all.peak"]
+        req = memory_states["requested_bytes.all.peak"]
+        print(f"**peak allocated/required memory: {alloc}, {req}")
+
+
+def get_return_memory(bsym):
+    assert bsym.sym.name == "return"
+    return_tensors_name = set()
+    res = 0
+    for x in bsym.flat_proxy_args:
+        if isinstance(x, TensorProxy) and x.name not in return_tensors_name:
+            res += x.numel * x.dtype.bytes
+            return_tensors_name.add(x.name)
+    return res
+
+
+@instantiate(dtypes=(thunder.float32,), devicetypes=(devices.DeviceType.CUDA,))
+def test_view_ops(executor, device: str, dtype: dtypes.dtype):
+    torch_dtype = ltorch.to_torch_dtype(dtype)
+    a = make_tensor((4,), device=device, dtype=torch_dtype, requires_grad=True)
+    b = make_tensor((2, 2), device=device, dtype=torch_dtype, requires_grad=True)
+
+    def bar(a, b):  # [4] [2,2]
+        a_1 = torch.unsqueeze(a, 0)  # [1,4]
+        a_2 = torch.unsqueeze(a_1, 1)  # [1,1,4]
+        a_3 = a_2.expand(2, 3, 4)  # [2,3,4]
+
+        b_1 = torch.reshape(b, (4,))  # [4]
+        b_2 = torch.unsqueeze(b_1, 0)  # [1,4]
+        b_3 = torch.unsqueeze(b_2, 1)  # [1,1,4]
+        b_4 = b_3.expand(2, 3, 4)  # [2,3,4]
+
+        result1 = a_2 + b_3
+        result2 = b_4 + a_3
+        return result1, result2
+
+    cbar = executor.make_callable(bar, disable_preprocessing=False)
+    with runtime_allocated_memory(device):
+        cbar(a, b)
+
+    traces = thunder.last_traces(cbar)
+    fwd_extrace = traces[0][-1]
+    max_mem_fwd = get_alloc_memory(fwd_extrace)
+    assert max_mem_fwd[0] == 144
+    assert sum(max_mem_fwd[1].values()) == get_return_memory(fwd_extrace.bound_symbols[-1])  # 144
+    bw_extrace = traces[1][-1]
+    max_mem_bw = get_alloc_memory(bw_extrace)
+    assert max_mem_bw[0] == 144
+    assert sum(max_mem_bw[1].values()) == get_return_memory(bw_extrace.bound_symbols[-1])  # 32
+
+    def bar1(a, b, c):  # [4], [1,4,4], [4,1,4]
+        a_1 = torch.unsqueeze(a, 0)  # [1,4]
+        a_2 = torch.unsqueeze(a_1, 1)  # [1,1,4]
+        a_3 = a_2.expand(1, 4, 4)
+        a_4 = a_2.expand(4, 1, 4)
+        return b + a_3, c + a_4
+
+    a = make_tensor((4,), device=device, dtype=torch_dtype)
+    b = make_tensor((1, 4, 4), device=device, dtype=torch_dtype)
+    c = make_tensor((4, 1, 4), device=device, dtype=torch_dtype)
+    cbar = executor.make_callable(bar1, disable_preprocessing=False)
+    with runtime_allocated_memory(device):
+        cbar(a, b, c)
+
+    traces = thunder.last_traces(cbar)
+    extrace = traces[-1]
+    alloc_mem = get_alloc_memory(extrace)
+    if isinstance(executor, nvFuserTestExecutor):
+        assert alloc_mem[0] == 272
+        assert sum(alloc_mem[1].values()) == get_return_memory(extrace.bound_symbols[-1])  # 128
+    if isinstance(executor, TorchTestExecutor):
+        assert alloc_mem[0] == 208
+        assert sum(alloc_mem[1].values()) == get_return_memory(extrace.bound_symbols[-1])  # 128
+
+    def bar2(a, b):  # [5,2], [2,2]
+        a_1, a_2, a_3 = torch.split(a, 2)
+        c = a_1 + b
+        d = a + a
+        return c, a_2, d
+
+    a = make_tensor((5, 2), device=device, dtype=torch_dtype)
+    b = make_tensor((2, 2), device=device, dtype=torch_dtype)
+    cbar = executor.make_callable(bar2, disable_preprocessing=False)
+
+    with runtime_allocated_memory(device):
+        cbar(a, b)
+
+    traces = thunder.last_traces(cbar)
+    extrace = traces[-1]
+    alloc_mem = get_alloc_memory(extrace)
+    if isinstance(executor, nvFuserTestExecutor):
+        assert alloc_mem[0] == 144
+        assert sum(alloc_mem[1].values()) == get_return_memory(extrace.bound_symbols[-1])  # 72
+    if isinstance(executor, TorchTestExecutor):
+        assert alloc_mem[0] == 112
+        assert sum(alloc_mem[1].values()) == get_return_memory(extrace.bound_symbols[-1])  # 72
+
+
+@instantiate(dtypes=(thunder.float32,), devicetypes=(devices.DeviceType.CUDA,))
+def test_nanogpt_block(executor, device, dtype):
+    import thunder.tests.nanogpt_model as nanogpt_model
+
+    tdtype = ltorch.to_torch_dtype(dtype)
+    make = partial(make_tensor, dtype=tdtype, device=device)
+
+    config = nanogpt_model.GPTConfig(dropout=0)
+    block = nanogpt_model.Block(config).to(device=device, dtype=tdtype)
+    cblock = executor.make_callable(block, disable_preprocessing=False)
+
+    with runtime_allocated_memory(device):
+        inp = make((2, config.block_size, config.n_embd))
+        result = cblock(inp)
+    with runtime_allocated_memory(device):
+        result.backward(torch.ones_like(result))
+    traces = thunder.last_traces(cblock)
+    fw_extrace = traces[0][-1]
+    bw_extrace = traces[1][-1]
+    fw_alloc_mem = get_alloc_memory(fw_extrace)
+    bw_alloc_mem = get_alloc_memory(bw_extrace)
+
+    if isinstance(executor, nvFuserTestExecutor):
+        assert fw_alloc_mem[0] == 271621120
+        # t67 is the expand result of ln_2_weight, and they are both return values in trace
+        # but for calculation we assume they share memory, so expect to subtract the size of t67
+        expected_return_calculated_mem = get_return_memory(fw_extrace.bound_symbols[-1]) - 4 * 2 * 1024 * 768
+        assert expected_return_calculated_mem == sum(fw_alloc_mem[1].values())
+
+        assert bw_alloc_mem[0] == 361783296
+        assert sum(bw_alloc_mem[1].values()) == get_return_memory(bw_extrace.bound_symbols[-1])
+    if isinstance(executor, TorchTestExecutor):
+        assert fw_alloc_mem[0] == 367057920
+        # same reason as above, expect to -t38+t37-t65-t67
+        expected_return_calculated_mem = (
+            get_return_memory(fw_extrace.bound_symbols[-1]) - 23 * 1024 * 1024 - 4 * 2 * 1024 * 768 * 2
+        )
+        assert expected_return_calculated_mem == sum(fw_alloc_mem[1].values())
+        assert bw_alloc_mem[0] == 412109824
+        assert sum(bw_alloc_mem[1].values()) == get_return_memory(bw_extrace.bound_symbols[-1])
