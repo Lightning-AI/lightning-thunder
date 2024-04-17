fcd99eab3f65cb66e8b438e38eac5024a8962c6e
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2024-04-17 17:35:11 +0300

    Guard thunder/distributed/transforms/ by torch.distributed.is_available()
    This is needed for a PyTorch build without distributed features.

--- thunder/distributed/transforms/__init__.py
+++ thunder/distributed/transforms/__init__.py
@@ -1,6 +1,10 @@
-from .ddp import optimize_allreduce_in_ddp_backward
-from .fsdp import FSDPCommBucketing
-
+import torch
+if torch.distributed.is_available():
+    from .ddp import optimize_allreduce_in_ddp_backward
+    from .fsdp import FSDPCommBucketing
+else:
+    optimize_allreduce_in_ddp_backward = None
+    FSDPCommBucketing = None
 
 __all__ = [
     "optimize_allreduce_in_ddp_backward",
