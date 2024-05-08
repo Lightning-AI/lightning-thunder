972f8d49e7a7d2458218d8a56bcd7ba0d9ebc75b
Yan Wang <wayan@nvidia.com>
2024-05-08 16:27:38 +0200

    Add test case followed comments

--- thunder/tests/test_randomness.py
+++ thunder/tests/test_randomness.py
@@ -33,26 +33,24 @@ def test_rng_state_prims(executor, device: str, _):
     import thunder.core.prims as prims
     import torch
 
-    dev = devices.to_device(device)
-
-    def func():
-        b = prims.get_rng_state(None, device=dev)
-        c = prims.get_rng_state(b, device=dev)
+    def func(device):
+        b = prims.get_rng_state(None, device=device)
+        c = prims.get_rng_state(b, device=device)
         seed, offset = prims.unpack_rng_state(b)
         new_state1 = prims.update_rng_state(seed, offset)
 
-        new_state1 = prims.set_rng_state(new_state1, dev)
-        new_state1_1 = prims.get_rng_state(new_state1, dev)
+        new_state1 = prims.set_rng_state(new_state1, device)
+        new_state1_1 = prims.get_rng_state(new_state1, device)
         state1_seed, state1_offset = prims.unpack_rng_state(new_state1_1)
         return b, c, seed, offset, new_state1_1, state1_seed, state1_offset
 
+    dev = devices.to_device(device)
     cuda_generator = torch.cuda.default_generators[dev.index]
     jfunc = thunder.jit(func, executors=executor.executors_list())
-    torch_device = thunder.core.devices.to_torch_device(dev)
-    with torch.random.fork_rng(devices=(torch_device,)):
+    with torch.random.fork_rng(devices=(device,)):
         cuda_generator.manual_seed(2)
         cuda_generator.set_offset(8)
-        ori_state, ori_state_1, ori_seed, ori_offset, state1, s1_seed, s1_offset = jfunc()
+        ori_state, ori_state_1, ori_seed, ori_offset, state1, s1_seed, s1_offset = jfunc(dev)
 
         cuda_generator.manual_seed(2)
         cuda_generator.set_offset(8)
@@ -66,3 +64,52 @@ def test_rng_state_prims(executor, device: str, _):
         assert_close(cuda_generator.get_state(), state1)
         assert_close(cuda_generator.initial_seed(), s1_seed)
         assert_close(cuda_generator.get_offset() // 4, s1_offset)
+
+
+@instantiate(
+    dtypes=(dtypes.float32, dtypes.float16, dtypes.float64),
+    devicetypes=(devices.DeviceType.CUDA,),
+    executors=(nvFuserExecutor,),
+)
+def test_uniform_philox_with_rng_state_prims(executor, device: str, dtype: dtypes.dtype):
+    import thunder.core.prims as prims
+    import torch
+
+    def func1(shape, dtype, device):
+        b = prims.get_rng_state(None, device=device)
+        seed, offset = prims.unpack_rng_state(b)
+        out1 = ltorch.uniform_philox(shape, device=device, dtype=dtype, seed=seed, offset=offset)
+        new_state1 = prims.update_rng_state(seed, offset)
+        new_state1 = prims.set_rng_state(new_state1, dev)
+
+        new_state1_1 = prims.get_rng_state(new_state1, device)
+        state1_seed, state1_offset = prims.unpack_rng_state(new_state1_1)
+        out2 = ltorch.uniform_philox(shape, device=device, dtype=dtype, seed=state1_seed, offset=state1_offset)
+        new_state2 = prims.update_rng_state(state1_seed, state1_offset)
+        new_state2_2 = prims.set_rng_state(new_state2, device)
+
+        return out1, out2
+
+    def func2(shape, dtype, device):
+        out1 = ltorch.uniform(shape, device=device, dtype=dtype)
+        out2 = ltorch.uniform(shape, device=device, dtype=dtype)
+        return out1, out2
+
+    dev = devices.to_device(device)
+
+    cuda_generator = torch.cuda.default_generators[dev.index]
+    jfunc1 = thunder.jit(func1, executors=executor.executors_list())
+    jfunc2 = thunder.jit(func2, executors=executor.executors_list())
+    shape = (2, 3, 4, 5)
+    with torch.random.fork_rng(devices=(device,)):
+        cuda_generator.manual_seed(2)
+        uniform_philox_o1, uniform_philox_o2 = jfunc1(shape, dtype, dev)
+        state1 = cuda_generator.get_state()
+
+        cuda_generator.manual_seed(2)
+        uniform_o1, uniform_o2 = jfunc2(shape, dtype, dev)
+        state2 = cuda_generator.get_state()
+
+        assert_close(state1, state2)
+        assert_close(uniform_o1, uniform_philox_o1)
+        assert_close(uniform_o2, uniform_philox_o2)
