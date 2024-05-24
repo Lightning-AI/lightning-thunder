89ba3da35515ff85302614c2cd8616d93ecffc27
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2024-05-24 19:26:43 +0300

    removed float8 from default test supported dtypes

--- thunder/tests/framework.py
+++ thunder/tests/framework.py
@@ -172,7 +172,7 @@ class nvFuserTestExecutor(TestExecutor):
     name = "nvfuser"
     supported_devicetypes = (devices.DeviceType.CUDA,)
     supported_dtypes = (
-        datatypes.floating,
+        *datatypes.float_math_dtypes,
         datatypes.bool8,
         datatypes.int32,
         datatypes.int64,
@@ -347,7 +347,7 @@ class ops:
         self.supported_devicetypes = set(filter_ci_devicetypes(self.supported_devicetypes))
 
         self.supported_dtypes = (
-            datatypes.resolve_dtypes(supported_dtypes) if supported_dtypes is not None else datatypes.all_dtypes
+            datatypes.resolve_dtypes(supported_dtypes) if supported_dtypes is not None else datatypes.all_dtypes - datatypes.float_8bit_dtypes
         )
 
         if supported_dtypes == NOTHING:
