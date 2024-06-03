62d093eb4f638a02d2fef4791e505f1dbb7af136
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2024-06-03 16:34:10 +0300

    Use '0' as default value for unset env var

--- thunder/core/decorators.py
+++ thunder/core/decorators.py
@@ -7,16 +7,13 @@ def avoid_torch_nccl_record_streams(func):
     """
 
     env_var = "TORCH_NCCL_AVOID_RECORD_STREAMS"
-    value = os.environ.get(env_var, None)
+    value = os.environ.get(env_var, "0")
 
     def wrapper(*args, **kwargs):
         try:
             os.environ[env_var] = "1"
             return func(*args, **kwargs)
         finally:
-            if value is not None:
-                os.environ[env_var] = value
-            else:
-                del os.environ[env_var]
+            os.environ[env_var] = value
 
     return wrapper
