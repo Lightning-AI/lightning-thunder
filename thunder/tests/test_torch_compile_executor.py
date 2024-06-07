551d6c151bafe0e72a89055c9193187184f8d380
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2024-06-07 11:57:23 +0300

    Add test_torch_compile_cat_nvfuser_phi2_tanh

--- thunder/tests/test_torch_compile_executor.py
+++ thunder/tests/test_torch_compile_executor.py
@@ -3,9 +3,11 @@ import torch
 from torch._dynamo import is_inductor_supported
 
 import thunder
-from thunder.executors.torch_compile import supported_ops, torch_compile_ex
+from thunder.executors.torch_compile import supported_ops, torch_compile_ex, torch_compile_cat_ex
 from thunder.executors.torchex import ex as pytorch_ex
-from thunder.tests.litgpt_model import GPT
+from thunder.executors.nvfuserex import nvfuserex
+from thunder.tests.litgpt_model import GPT, Config
+from thunder.tests .framework import requiresCUDA
 
 
 def test_supported_ops_are_in_pytorch_executor():
@@ -23,3 +25,19 @@ def test_torch_compile_litgpt():
     # a single torch.compile region. normally you would want to enable sdpa too
     assert "TorchCompile0" in forward_trace
     assert "TorchCompile1" not in forward_trace
+
+# Testing the following issue:
+# https://github.com/Lightning-AI/lightning-thunder/issues/292 The issue was
+# that the CSE pass was not being run correctly on the TorchCompile region.
+# Here we test that everything works as expected.
+@pytest.mark.skipif(not is_inductor_supported(), reason="inductor unsupported")
+@requiresCUDA
+def test_torch_compile_cat_nvfuser_phi2_tanh():
+    device = torch.device("cuda")
+    config = Config.from_name("phi-2", n_layer=1, gelu_approximate="tanh")
+    with device:
+        model = GPT(config).to(torch.bfloat16)
+    x = torch.randint(model.max_seq_length, (5, 5), device=device)
+    cmodel = thunder.jit(model, executors=[torch_compile_cat_ex, nvfuserex])
+    logits = cmodel(x)
+    logits.sum().backward()
