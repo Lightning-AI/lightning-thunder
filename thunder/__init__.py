4a951ac68e0e90775e99fd5aec7ab0943ec47edf
Thomas Viehmann <tv.code@beamnet.de>
2024-06-07 11:02:00 +0200

    add missing transform_trace

--- thunder/__init__.py
+++ thunder/__init__.py
@@ -606,7 +606,7 @@ def jit(
                 computation_trc = transform.transform_trace(computation_trc, executors_list=cd.executors_list)
                 extraces.append(computation_trc)
                 if backward_trc is not None:
-                    backward_trc = transform(backward_trc, executors_list=cd.executors_list)
+                    backward_trc = transform.transform_trace(backward_trc, executors_list=cd.executors_list)
                     backward_traces.append(backward_trc)
 
             comp = computation_trc.python_callable()
