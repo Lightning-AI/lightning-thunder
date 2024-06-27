28817c1d43c68e8b28113929683163c4982a8d9e
Thomas Viehmann <tv.code@beamnet.de>
2024-06-27 13:28:19 +0200

    fix lint

--- thunder/tests/test_jit_general.py
+++ thunder/tests/test_jit_general.py
@@ -685,7 +685,6 @@ def test_litgpt_variants(name, device):
         torch.testing.assert_close(param1.grad, param2.grad, rtol=1e-2, atol=1e-2)
 
 
-
 @pytest.mark.skipif(
     version_between(torch.__version__, min_ver="2.5.0a0", max_ver="2.5.0a99"),
     reason="https://github.com/pytorch/pytorch/issues/129579",
