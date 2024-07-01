e08c46e7562e748e784cae3dc12403c5e2aac14e
Kaeun Kim <k223kim@uwaterloo.ca>
2024-07-01 14:27:11 +0900

    feat: fix

--- thunder/executors/pythonex.py
+++ thunder/executors/pythonex.py
@@ -50,11 +50,8 @@ def _check_tensor_shape_and_metadata_impl(
 ) -> None:
     assert isinstance(t, torch.Tensor), f"expected Tensor, got {type(t).__name__}"
     assert (
-        tuple(t.shape) == shape
-        and str(t.device) == device.device_str()
-        and t.dtype == dtype
-        and t.requires_grad == requires_grad
-    ), f"expected tensor with {shape}, {device.device_str()}, {dtype}, {requires_grad=}, got {tuple(t.shape)}, {str(t.device_str())}, {t.dtype}, {requires_grad}"
+        tuple(t.shape) == shape and str(t.device) == device and t.dtype == dtype and t.requires_grad == requires_grad
+    ), f"expected tensor with {shape}, {device}, {dtype}, {requires_grad=}, got {tuple(t.shape)}, {str(t.device)}, {t.dtype}, {requires_grad}"
 
 
 check_tensor_shape_and_metadata = ex.register_operator(
