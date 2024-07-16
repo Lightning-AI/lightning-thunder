b8ca656d9c02982e204c449a8877832371701d7b
Yan Wang <wayan@nvidia.com>
2024-07-16 10:51:07 +0200

    tmp rm test

--- thunder/tests/test_fallback_to_torchops.py
+++ thunder/tests/test_fallback_to_torchops.py
@@ -28,134 +28,134 @@ skip_ops = [
 ]
 skip_ops1 = [get_opinfo("conv2d"), get_opinfo("linear"), get_opinfo("adaptive_avg_pool2d"), get_opinfo("max_pool2d")]
 disable_opinfos = _unsupported_opinfos + _fallback_opinfos1 + _fallback_opinfos2 + skip_ops + skip_ops1
-tmp1 = dict(thunder.core.jit_ext._general_jit_lookaside_map)
-list(tmp1.pop(k.torch_reference, None) for k in disable_opinfos)
-tmp2 = dict(thunder.torch._torch_to_thunder_function_map)
-list(tmp2.pop(k.torch_reference, None) for k in disable_opinfos)
-tmp3 = dict(thunder.core.jit_ext._minimal_lookaside_map)
-list(tmp3.pop(k.torch_reference, None) for k in disable_opinfos)
+# tmp1 = dict(thunder.core.jit_ext._general_jit_lookaside_map)
+# list(tmp1.pop(k.torch_reference, None) for k in disable_opinfos)
+# tmp2 = dict(thunder.torch._torch_to_thunder_function_map)
+# list(tmp2.pop(k.torch_reference, None) for k in disable_opinfos)
+# tmp3 = dict(thunder.core.jit_ext._minimal_lookaside_map)
+# list(tmp3.pop(k.torch_reference, None) for k in disable_opinfos)
 
 
-@ops(_unsupported_opinfos, supported_dtypes=(dtypes.float32,), supported_executors=(TorchExecutor,))
-def test_fallback_exception(
-    op: OpInfo,
-    device: str,
-    dtype: dtypes.dtype,
-    executor,
-    comp,
-):
-    # with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp, clear=True):
-    with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
-        with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
-            with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
-                for sample in op.sample_inputs(device, dtype, requires_grad=True):
-                    comp = sample.comp if sample.comp is not None else comp
-                    tfn = thunder.jit(
-                        op.torch_reference,
-                        executors=executor.executors_list(),
-                    )
-                    with pytest.raises(
-                        NotImplementedError, match="^Exception encountered when doing automatic registration"
-                    ):
-                        thunder_result = tfn(*sample.args, **sample.kwargs)
+# @ops(_unsupported_opinfos, supported_dtypes=(dtypes.float32,), supported_executors=(TorchExecutor,))
+# def test_fallback_exception(
+#     op: OpInfo,
+#     device: str,
+#     dtype: dtypes.dtype,
+#     executor,
+#     comp,
+# ):
+#     # with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp, clear=True):
+#     with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
+#         with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
+#             with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
+#                 for sample in op.sample_inputs(device, dtype, requires_grad=True):
+#                     comp = sample.comp if sample.comp is not None else comp
+#                     tfn = thunder.jit(
+#                         op.torch_reference,
+#                         executors=executor.executors_list(),
+#                     )
+#                     with pytest.raises(
+#                         NotImplementedError, match="^Exception encountered when doing automatic registration"
+#                     ):
+#                         thunder_result = tfn(*sample.args, **sample.kwargs)
 
 
-def test_torch_inplace_ops():
-    def func1(x):
-        return torch.relu_(x)
+# def test_torch_inplace_ops():
+#     def func1(x):
+#         return torch.relu_(x)
 
-    def func2(x):
-        return torch.nn.functional.selu(x, inplace=True)
+#     def func2(x):
+#         return torch.nn.functional.selu(x, inplace=True)
 
-    with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, {}, clear=True):
-        with patch.dict(thunder.torch._torch_to_thunder_function_map, {}, clear=True):
-            with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, {}, clear=True):
-                x = torch.rand((4, 4)).cuda()
-                jfn = thunder.jit(func1, executors=[thunder.pytorch_executor])
-                assert torch.relu_ not in thunder.torch._torch_to_thunder_function_map
+#     with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, {}, clear=True):
+#         with patch.dict(thunder.torch._torch_to_thunder_function_map, {}, clear=True):
+#             with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, {}, clear=True):
+#                 x = torch.rand((4, 4)).cuda()
+#                 jfn = thunder.jit(func1, executors=[thunder.pytorch_executor])
+#                 assert torch.relu_ not in thunder.torch._torch_to_thunder_function_map
 
-                jfn = thunder.jit(func2, executors=[thunder.pytorch_executor])
-                with pytest.raises(NotImplementedError, match="has inplace=True, please use manual registration$"):
-                    thunder_result = jfn(x)
-
-
-@ops(_fallback_opinfos1, supported_dtypes=(dtypes.float32,), supported_executors=(TorchExecutor,))
-def test_fallback_forward(op, device, dtype, executor, comp):
-    with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
-        with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
-            with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
-                for sample in op.sample_inputs(device, dtype):
-                    comp = sample.comp if sample.comp is not None else comp
-                    tfn = thunder.jit(
-                        op.torch_reference,
-                        executors=executor.executors_list(),
-                    )
-                    thunder_result = tfn(*sample.args, **sample.kwargs)
-                    torch_result = op.torch_reference(*sample.args, **sample.kwargs)
-                    comp(thunder_result, torch_result)
-                    fwd_trc = thunder.last_traces(tfn)[-1]
-                    assert any(
-                        bsym.sym.name.endswith(op.name) and not bsym.subsymbols for bsym in fwd_trc.bound_symbols
-                    )
+#                 jfn = thunder.jit(func2, executors=[thunder.pytorch_executor])
+#                 with pytest.raises(NotImplementedError, match="has inplace=True, please use manual registration$"):
+#                     thunder_result = jfn(x)
 
 
-@ops(_fallback_opinfos2, supported_dtypes=(dtypes.float32,), supported_executors=(TorchExecutor,))
-def test_fallback_backward(op, device, dtype, executor, comp):
-    with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
-        with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
-            with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
-                for sample in op.sample_inputs(device, dtype, requires_grad=True):
-                    comp = sample.comp if sample.comp is not None else comp
-                    tfn = thunder.jit(
-                        op.torch_reference,
-                        executors=executor.executors_list(),
-                    )
-                    thunder_result = tfn(*sample.args, **sample.kwargs)
-
-                    bwd_trc = thunder.last_backward_traces(tfn)[-1]
-                    vjp_op_name = f"{op.name}_vjp"
-                    assert any(bsym.sym.name == vjp_op_name for bsym in bwd_trc.bound_symbols)
-
-
-@instantiate(dtypes=(thunder.float32,), devicetypes=(devices.DeviceType.CUDA,), executors=(TorchExecutor,))
-def test_nanogpt_block(executor, device, dtype):
-    with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
-        with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
-            with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
-                import thunder.tests.nanogpt_model as nanogpt_model
-
-                tdtype = thunder.torch.to_torch_dtype(dtype)
-                make = partial(make_tensor, dtype=tdtype, device=device)
-
-                config = nanogpt_model.GPTConfig(dropout=0)
-                model = nanogpt_model.Block(config).to(device=device, dtype=tdtype)
-                jitted = executor.make_callable(model)
-
-                x = make((2, config.block_size, config.n_embd))
-
-                cache_entry, _, _ = thunder.compile_data(jitted).get_computation_and_inputs(x)
-                bwd_trcs = cache_entry.backward_traces
-                for op in skip_ops:
-                    vjp_op_name = f"{op.name}_vjp"
-                    assert any(bsym.sym.name == vjp_op_name for bsym in bwd_trcs[-1].bound_symbols)
+# @ops(_fallback_opinfos1, supported_dtypes=(dtypes.float32,), supported_executors=(TorchExecutor,))
+# def test_fallback_forward(op, device, dtype, executor, comp):
+#     with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
+#         with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
+#             with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
+#                 for sample in op.sample_inputs(device, dtype):
+#                     comp = sample.comp if sample.comp is not None else comp
+#                     tfn = thunder.jit(
+#                         op.torch_reference,
+#                         executors=executor.executors_list(),
+#                     )
+#                     thunder_result = tfn(*sample.args, **sample.kwargs)
+#                     torch_result = op.torch_reference(*sample.args, **sample.kwargs)
+#                     comp(thunder_result, torch_result)
+#                     fwd_trc = thunder.last_traces(tfn)[-1]
+#                     assert any(
+#                         bsym.sym.name.endswith(op.name) and not bsym.subsymbols for bsym in fwd_trc.bound_symbols
+#                     )
 
 
-@instantiate(dtypes=(thunder.float64,), devicetypes=(devices.DeviceType.CUDA,), executors=(TorchExecutor,))
-def test_alexnet(executor, device, dtype):
-    torchvision = pytest.importorskip("torchvision")
+# @ops(_fallback_opinfos2, supported_dtypes=(dtypes.float32,), supported_executors=(TorchExecutor,))
+# def test_fallback_backward(op, device, dtype, executor, comp):
+#     with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
+#         with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
+#             with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
+#                 for sample in op.sample_inputs(device, dtype, requires_grad=True):
+#                     comp = sample.comp if sample.comp is not None else comp
+#                     tfn = thunder.jit(
+#                         op.torch_reference,
+#                         executors=executor.executors_list(),
+#                     )
+#                     thunder_result = tfn(*sample.args, **sample.kwargs)
 
-    with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
-        with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
-            with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
-                tdtype = thunder.torch.to_torch_dtype(dtype)
-                model = torchvision.models.alexnet(weights=None).to(device=device, dtype=tdtype)
-                model = model.train()
+#                     bwd_trc = thunder.last_backward_traces(tfn)[-1]
+#                     vjp_op_name = f"{op.name}_vjp"
+#                     assert any(bsym.sym.name == vjp_op_name for bsym in bwd_trc.bound_symbols)
 
-                jitted = executor.make_callable(model)
-                x = make_tensor((1, 3, 224, 224), dtype=tdtype, device=device)
 
-                cache_entry, _, _ = thunder.compile_data(jitted).get_computation_and_inputs(x)
-                bwd_trcs = cache_entry.backward_traces
-                for op in skip_ops1:
-                    vjp_op_name = f"{op.name}_vjp"
-                    assert any(bsym.sym.name == vjp_op_name for bsym in bwd_trcs[-1].bound_symbols)
+# @instantiate(dtypes=(thunder.float32,), devicetypes=(devices.DeviceType.CUDA,), executors=(TorchExecutor,))
+# def test_nanogpt_block(executor, device, dtype):
+#     with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
+#         with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
+#             with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
+#                 import thunder.tests.nanogpt_model as nanogpt_model
+
+#                 tdtype = thunder.torch.to_torch_dtype(dtype)
+#                 make = partial(make_tensor, dtype=tdtype, device=device)
+
+#                 config = nanogpt_model.GPTConfig(dropout=0)
+#                 model = nanogpt_model.Block(config).to(device=device, dtype=tdtype)
+#                 jitted = executor.make_callable(model)
+
+#                 x = make((2, config.block_size, config.n_embd))
+
+#                 cache_entry, _, _ = thunder.compile_data(jitted).get_computation_and_inputs(x)
+#                 bwd_trcs = cache_entry.backward_traces
+#                 for op in skip_ops:
+#                     vjp_op_name = f"{op.name}_vjp"
+#                     assert any(bsym.sym.name == vjp_op_name for bsym in bwd_trcs[-1].bound_symbols)
+
+
+# @instantiate(dtypes=(thunder.float64,), devicetypes=(devices.DeviceType.CUDA,), executors=(TorchExecutor,))
+# def test_alexnet(executor, device, dtype):
+#     torchvision = pytest.importorskip("torchvision")
+
+#     with patch.dict(thunder.core.jit_ext._general_jit_lookaside_map, tmp1, clear=True):
+#         with patch.dict(thunder.torch._torch_to_thunder_function_map, tmp2, clear=True):
+#             with patch.dict(thunder.core.jit_ext._minimal_lookaside_map, tmp3, clear=True):
+#                 tdtype = thunder.torch.to_torch_dtype(dtype)
+#                 model = torchvision.models.alexnet(weights=None).to(device=device, dtype=tdtype)
+#                 model = model.train()
+
+#                 jitted = executor.make_callable(model)
+#                 x = make_tensor((1, 3, 224, 224), dtype=tdtype, device=device)
+
+#                 cache_entry, _, _ = thunder.compile_data(jitted).get_computation_and_inputs(x)
+#                 bwd_trcs = cache_entry.backward_traces
+#                 for op in skip_ops1:
+#                     vjp_op_name = f"{op.name}_vjp"
+#                     assert any(bsym.sym.name == vjp_op_name for bsym in bwd_trcs[-1].bound_symbols)
