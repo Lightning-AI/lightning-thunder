5bfc1a552908e9d805bea957621a827fc8d0bcf1
kshitij12345 <kshitijkalambarkar@gmail.com>
2024-07-18 15:33:03 +0200

    undo backward setup changes

--- thunder/benchmarks/targets.py
+++ thunder/benchmarks/targets.py
@@ -635,28 +635,25 @@ def backward_only(fn: Callable, *args, **kwargs):
         A tuple of the backward function and the setup function
         that returns the arguments for the backward function.
     """
+    result = fn(*args, **kwargs)
+    result = thunder.core.utils.sequencify(result)
 
-    # backward setup takes care of running the forward, saving the relevant context for backward
-    # and returning the `grads` for output.
+    forward_inputs = thunder.core.pytree.tree_flatten((args, kwargs))[0]
+    forward_inputs = list(filter(lambda x: isinstance(x, torch.Tensor) and x.requires_grad, forward_inputs))
+    backwardable_tensor_result = list(filter(lambda x: isinstance(x, torch.Tensor) and x.requires_grad, result))
+
+    # Capture metadata for backward to avoid keeping the result in memory
+    backwardable_result_metadata = [(r.dtype, r.device, r.shape) for r in backwardable_tensor_result]
+
     def backward_setup():
-        result = fn(*args, **kwargs)
-        result = thunder.core.utils.sequencify(result)
-
-        forward_inputs = thunder.core.pytree.tree_flatten((args, kwargs))[0]
-        forward_inputs = list(filter(lambda x: isinstance(x, torch.Tensor) and x.requires_grad, forward_inputs))
-        backwardable_tensor_result = list(filter(lambda x: isinstance(x, torch.Tensor) and x.requires_grad, result))
-
-        # Capture metadata for backward to avoid keeping the result in memory
-        backwardable_result_metadata = [(r.dtype, r.device, r.shape) for r in backwardable_tensor_result]
         output_grads = []
         for dtype, device, shape in backwardable_result_metadata:
             torch_dtype = thunder.torch.to_torch_dtype(dtype)
             torch_device = thunder.core.devices.to_torch_device(device)
             output_grads.append(make_tensor(shape, dtype=torch_dtype, device=torch_device, requires_grad=False))
-        return result, forward_inputs, output_grads
+        return output_grads
 
-    # Actually do the backward pass.
-    def backward_fn(result, forward_inputs, output_grads):
+    def backward_fn(*output_grads):
         for i in forward_inputs:
             i.grad = None
 
