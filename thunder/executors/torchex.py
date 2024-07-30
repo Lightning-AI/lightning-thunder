a688ddee6066d678432ee9dad85a1df17629e3d8
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2024-07-30 10:46:06 +0300

    functorch.einops might exists; check for einops._backends

--- thunder/executors/torchex.py
+++ thunder/executors/torchex.py
@@ -2104,6 +2104,7 @@ _register_implementation(prims.item, item, checker=_always_executable)
 
 
 has_einops = importlib.util.find_spec("einops") is not None
+has_einops &= importlib.util.find_spec("einops._backends") is not None
 if has_einops:
     import einops
     from einops._backends import TorchBackend
