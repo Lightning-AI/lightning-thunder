37b116eab8f6873e33bbeb462e481eaba6b02e0b
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2024-07-31 11:18:23 +0300

    Add guards and pre-post symbol debug actions

--- thunder/dev_utils/debug_transform.py
+++ thunder/dev_utils/debug_transform.py
@@ -1,51 +1,72 @@
-from thunder.core.trace import TraceCtx as Trace, from_trace, TraceProvenance
-import thunder
+from collections.abc import Callable
 from functools import partial
-from thunder.core.symbol import Symbol
+import time
+
+from thunder.core.trace import TraceCtx, from_trace, TraceProvenance
+import thunder
+from thunder.core.symbol import Symbol, BoundSymbol
 from thunder.dev_utils.utils import NON_COMPUTATION_PRIMS
 
 
-# NOTE: `computation_bsym, debug_bsym, callback` are mandatory keyword-only arguments.
-def debug_impl(*args, computation_bsym, debug_bsym, callback, **kwargs):
-    output = callback(computation_bsym, *args, **kwargs)
-    thunder.core.utils.check_type(output, str)
-    debug_bsym.header = output
+def create_debug_boundsymbol(name: str, bsym: BoundSymbol, call_ctx: Callable):
+    debug_sym = Symbol(name, lambda *_: None, is_prim=True)
+    debug_bsym = debug_sym.bind(*bsym.args, output=None, _call_ctx={name: partial(call_ctx, bsym)}, **bsym.kwargs)
+    return debug_bsym
 
 
 class DebugTransform(thunder.core.transforms.Transform):
-    def __init__(self, callback):
-        self.callback = callback
+    def __init__(
+        self,
+        *,
+        pre_callback: Callable[[tuple[BoundSymbol, ...]], str] | None,
+        post_callback: Callable[[tuple[BoundSymbol, ...]], str] | None,
+        guard: Callable[[], bool] | None = None,
+    ):
+        self.pre_callback = pre_callback
+        self.post_callback = post_callback
+        self.guard = guard if guard is not None else lambda _: True
 
-    def transform_trace_post_optimization(self, trace: Trace, **kwargs) -> Trace:
+    def transform_trace_post_optimization(self, trace: TraceCtx, **kwargs) -> TraceCtx:
+        start_time_ns = time.perf_counter_ns()
         debug_trace = from_trace(trace)
-        cnt = 1
-        for bound_symbol in trace.bound_symbols:
-            if bound_symbol.sym.id in NON_COMPUTATION_PRIMS:
-                # Just append the symbol.
-                debug_trace.bound_symbols.append(bound_symbol)
+        debug_counter = 1
+
+        new_bsyms: list[BoundSymbol] = []
+        for bsym in trace.bound_symbols:
+            sym_name = bsym.sym.name
+
+            if bsym.sym.id in NON_COMPUTATION_PRIMS:
+                new_bsyms.append(bsym)
                 continue
 
-            # we need unique name for each symbol.
-            debug_sym_name = f"debug_{bound_symbol.sym.name}_{cnt}"
+            if self.guard(bsym) and self.pre_callback is not None:
 
-            def bind_postprocess(bsym) -> None:
-                # This dict is then used by trace.python_ctx() to resolve the
-                # BoundSymbol to the actual function.
-                bsym._call_ctx = {
-                    debug_sym_name: partial(
-                        debug_impl, computation_bsym=bound_symbol, debug_bsym=bsym, callback=self.callback
-                    )
-                }
+                def _pre_call_ctx(bsym, *args, **kwargs):
+                    out = self.pre_callback(bsym, *args, **kwargs)
+                    thunder.core.utils.check_type(out, str)
+                    pre_debug_bsym.header = out
 
-            # Create a new debug symbol for this bsym.
-            debug_sym = Symbol(
-                debug_sym_name, lambda *args, **kwargs: None, is_prim=True, _bind_postprocess=bind_postprocess
-            )
-            debug_bsym = debug_sym.bind(*bound_symbol.args, output=None, **bound_symbol.kwargs)
+                pre_debug_name = f"debug_pre_{sym_name}{debug_counter}"
+                pre_debug_bsym = create_debug_boundsymbol(pre_debug_name, bsym, _pre_call_ctx)
+                new_bsyms.append(pre_debug_bsym)
 
-            debug_trace.bound_symbols.append(debug_bsym)
-            debug_trace.bound_symbols.append(bound_symbol)
-            cnt += 1
+            new_bsyms.append(bsym)
 
-        debug_trace.set_provenance(TraceProvenance("Debug Transform"))
+            if self.guard(bsym) and self.post_callback is not None:
+
+                def _post_call_ctx(bsym, *args, **kwargs):
+                    out = self.post_callback(bsym, *args, **kwargs)
+                    thunder.core.utils.check_type(out, str)
+                    post_debug_bsym.header = out
+
+                post_debug_name = f"debug_post_{sym_name}{debug_counter}"
+                post_debug_bsym = create_debug_boundsymbol(post_debug_name, bsym, _post_call_ctx)
+                new_bsyms.append(post_debug_bsym)
+
+        debug_trace.bound_symbols = new_bsyms
+        elapsed_time_ns = time.perf_counter_ns() - start_time_ns
+
+        debug_trace.set_provenance(TraceProvenance(f"Debug trace (took {elapsed_time_ns * 1e-6:.2f} milliseconds)"))
+
+        debug_counter += 1
         return debug_trace
