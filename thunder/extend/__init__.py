836b159eba4af54db8ceb41c0ca1129e0302b4cc
apaz <aarpazdera@gmail.com>
2024-08-02 19:10:17 +0000

    Added instance check and new error message.

--- thunder/extend/__init__.py
+++ thunder/extend/__init__.py
@@ -460,6 +460,8 @@ def resolve_executors(executors: None | Sequence[Executor | str]) -> tuple[Execu
                 resolved_executors.append(ex)
         elif e is None:
             raise ValueError(f"The executor list cannot contain None. Executor list: {executors}")
+        elif not isinstance(e, Executor):
+            raise ValueError(f"An object of type {type(e)} is not a valid Executor. Executor list: {executors}")
         else:
             resolved_executors.append(e)
 
