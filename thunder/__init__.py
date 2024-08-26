7f0a0e6d060fc3d8d8269a4381b7422b18cd606e
Yan Wang <kiya00wy@gmail.com>
2024-08-26 14:09:13 +0200

    Update thunder/__init__.py
    Co-authored-by: Thomas Viehmann <tv.code@beamnet.de>

--- thunder/__init__.py
+++ thunder/__init__.py
@@ -1019,7 +1019,7 @@ def print_auto_registered_torch_ops(fn: Callable, /) -> set[str] | None:
     op_names = set()
     trc = last_traces(fn)[0]
     for bsym in trc.bound_symbols:
-        if (meta_func_name := getattr(bsym.sym.meta, "__name__", None)) and meta_func_name == "meta_func":
+        if getattr(bsym.sym.meta, "__name__", None) == "meta_func":
             op_names.add(bsym.sym.id)
     return op_names if op_names else None
 
