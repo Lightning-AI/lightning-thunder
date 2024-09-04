c42427b690ac4f74819f06871b928045069e615c
Yan Wang <wayan@nvidia.com>
2024-09-04 16:08:17 +0200

    only check tensorproxy

--- thunder/distributed/transforms/fsdp_v2.py
+++ thunder/distributed/transforms/fsdp_v2.py
@@ -62,7 +62,7 @@ class FSDPParamUnpaddingVisitor:
             self.swap_map[variableify(padded_tensor)] = unpadded_tensor
             return VISIT_TYPE.INSERT_AFTER
 
-        if not any(variableify(a) in self.swap_map for a in bsym.flat_proxy_args):
+        if not any(variableify(a) in self.swap_map for a in bsym.flat_proxy_args if isinstance(a, TensorProxy)):
             return VISIT_TYPE.NO_OP
         updated_bsym = bsym.from_bsym_swap_proxies(self.swap_map)
         get_tracectx().scopes[-1].append(updated_bsym)
