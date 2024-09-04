57d51968e67f5e6fd73572668129392a5842f5a8
Yan Wang <wayan@nvidia.com>
2024-09-04 10:49:28 +0200

    fix tests

--- thunder/core/prims.py
+++ thunder/core/prims.py
@@ -2291,7 +2291,10 @@ def _elementwise_binary_meta_factory(
         # Checks same device
         utils.check_same_device(a, b, allow_cpu_scalar_tensors=True)
 
-        tensor = a if isinstance(a, TensorProxy) else b
+        if isinstance(a, TensorProxy) and isinstance(b, TensorProxy):
+            tensor = a if (isinstance(a, TensorProxy) and not utils.is_cpu_scalar_tensor(a)) else b
+        else:
+            tensor = a if isinstance(a, TensorProxy) else b
         requires_grad = (isinstance(a, TensorProxy) and a.requires_grad) or (
             isinstance(b, TensorProxy) and b.requires_grad
         )
