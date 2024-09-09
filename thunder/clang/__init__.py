a6c3e3841d6bebc4cf88e33c6a9986d4153d9ec3
Yinmin Liu <yinmin@YL-MBA.local>
2024-09-09 11:28:46 -0400

    fixed a bug occured, when replacing .numel

--- thunder/clang/__init__.py
+++ thunder/clang/__init__.py
@@ -1067,9 +1067,9 @@ def reshape(a: TensorLike, shape: Sequence[int]) -> TensorLike:
 
     # Constructs the inferred shape, replacing -1 with the necessary length
     utils.check(
-        a.numel() % numel == 0, lambda: f"Trying to reshape, but can't infer how to reshape {a.shape} to {shape}"
+        a.numel % numel == 0, lambda: f"Trying to reshape, but can't infer how to reshape {a.shape} to {shape}"
     )
-    remaining = a.numel() // numel
+    remaining = a.numel // numel
     shape = list(shape)
     shape[neg_one_idx] = remaining
     shape = tuple(shape)
