a639260a37596c977fda575257579624035e8c66
Thomas Viehmann <tv.code@beamnet.de>
2024-09-23 12:11:12 +0200

    cut one trace->callable->trace in the main augmented forward pass (#1164)

--- thunder/tests/distributed/test_fsdp.py
+++ thunder/tests/distributed/test_fsdp.py
@@ -126,8 +126,8 @@ class FSDPTest(DistributedParallelTestCase):
         #       in the original trace and are inputs to all_gather, the unshard are the outputs fo the corresponding wait
         #       If you fix this to be dynamically discerned, you'll be my hero.
         sharded_param_names = ("t_net1_weight", "t_net2_weight")
-        # t3 and t16 are all-gather'ed t_net1_weight and t_net2_weight, respectively.
-        unshard_param_names = ("t3", "t16")
+        # t5 and t20 are all-gather'ed t_net1_weight and t_net2_weight, respectively.
+        unshard_param_names = ("t5", "t20")
         result_saved_for_bwd = [x.name for x in fwd_trc.bound_symbols[-1].args[1][0]]
         self.assertTrue(all(t not in sharded_param_names for t in result_saved_for_bwd))
         self.assertTrue(all(t in result_saved_for_bwd for t in unshard_param_names))
