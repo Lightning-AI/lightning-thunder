f194118792678dcbebfc280c324ee47a8b92ffa3
Yan Wang <wayan@nvidia.com>
2024-09-24 20:36:08 +0200

    Skip thunder/tests/test_examine_memory.py:test_nanogpt_block on Hopper because estimated memory usage is not the same as actual peak memory usage

--- thunder/tests/test_examine_memory.py
+++ thunder/tests/test_examine_memory.py
@@ -98,6 +98,14 @@ def test_view_ops():
 
 @requiresCUDA
 def test_nanogpt_block():
+    from thunder.tests.test_sdpaex_executor import device_version_support, CudaVersion
+
+    torch_device = torch.device("cuda")
+    # The estimated memory usage is not the same as actual peak memory usage on Hopper
+    if device_version_support(torch_device, CudaVersion(9, 0), CudaVersion(9, 0)):
+        pytest.skip(
+            f"the estimated memory usage is not the same as actual peak memory usage on {torch.cuda.get_device_name()}"
+        )
     import thunder.tests.nanogpt_model as nanogpt_model
 
     config = nanogpt_model.GPTConfig(dropout=0)
