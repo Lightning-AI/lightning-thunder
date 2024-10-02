3a1bf2d6afcf46a87e4b200f91b1fc4caff1aa95
Masaki Kozuki <mkozuki@nvidia.com>
2024-10-02 18:59:52 +0900

    [functionalization] boundary check before accessing (#1246)

--- thunder/tests/test_inplace_functionalization.py
+++ thunder/tests/test_inplace_functionalization.py
@@ -758,3 +758,21 @@ def test_reshape_flatten_error_out(executor, device, _):
     x = make_tensor((3, 2, 4), device=device, dtype=torch.float32)
     jitted = executor.make_callable(f_with_clone)
     jitted(x)
+
+
+# ref: https://github.com/Lightning-AI/lightning-thunder/issues/1236
+@instantiate(dtypes=NOTHING)
+def test_unused_view_input(executor, device, _):
+
+    def f(a, x, unused):
+        return a.exp().add_(x)
+
+    x = make_tensor((2, 2), device=device, dtype=torch.float32)
+    a = x.clone()
+    unused = x[0]
+
+    jitted = executor.make_callable(f)
+
+    expected = f(a, x, unused)
+    actual = jitted(a, x, unused)
+    torch.testing.assert_close(actual, expected)
