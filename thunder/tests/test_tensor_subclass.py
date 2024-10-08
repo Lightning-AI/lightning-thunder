74b6ff7ee293b1d0b4122b122b7eb586b144493d
Masaki Kozuki <mkozuki@nvidia.com>
2024-10-08 13:22:48 +0900

    remove torchao float8 test for now
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- thunder/tests/test_tensor_subclass.py
+++ thunder/tests/test_tensor_subclass.py
@@ -3,13 +3,12 @@ import pytest
 from typing import TYPE_CHECKING, cast
 
 import torch
-import torch.nn as nn
 from torch.utils import _pytree as pytree
 
 from thunder.core import devices
 from thunder.core import dtypes
-from thunder.tests.framework import DynamoThunderExecutor
 from thunder.tests.framework import instantiate
+from thunder.tests.framework import DynamoThunderExecutor
 from thunder.tests.framework import nvFuserExecutor
 from thunder.tests.framework import TorchExecutor
 from thunder.tests.framework import TorchCompileExecutor
@@ -117,44 +116,3 @@ def test_subclass(executor, device, _):
     else:
         actual = jitted(x, y)
         torch.testing.assert_close(actual, expected)
-
-
-@instantiate(
-    executors=(
-        DynamoThunderExecutor,
-        nvFuserExecutor,
-    ),
-    devicetypes=(devices.DeviceType.CUDA,),
-    dtypes=(dtypes.bfloat16,),
-)
-def test_torchao_float8_linear(executor, device, dtype):
-    pytest.importorskip("torchao")
-    from torchao.float8 import convert_to_float8_training
-
-    torch_device = devices.to_torch_device(device)
-    torch_dtype = dtypes.to_torch_dtype(dtype)
-
-    if (capability := torch.cuda.get_device_capability(torch_device)) < (8, 9):
-        pytest.skip(f"Requires capability>=(8, 9) but  {capability=}")
-
-    model = nn.Sequential(
-        nn.Linear(64, 32),
-        nn.GELU(approximate="tanh"),
-        nn.Linear(32, 16),
-    ).to(device=torch_device, dtype=torch_dtype)
-    model = convert_to_float8_training(model)
-    jitted = executor.make_callable(model)
-    optimizer = torch.optim.AdamW(jitted.parameters())
-
-    if executor == DynamoThunderExecutor:
-        for _ in range(2):
-            optimizer.zero_grad()
-            x = make_tensor((16, 64), device=torch_device, dtype=torch_dtype, requires_grad=True)
-            out = jitted(x)
-            out.backward(torch.rand_like(out))
-
-            optimizer.step()
-    else:
-        x = make_tensor((16, 64), device=torch_device, dtype=torch_dtype, requires_grad=True)
-        with pytest.raises(AttributeError, match="Unknown attribute stride"):
-            _ = jitted(x)
