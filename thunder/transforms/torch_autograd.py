f82300c026502a769e0cd948458393c38a1d02dc
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2024-10-18 16:31:16 +0300

    Add get_backward, set_backward

--- thunder/transforms/torch_autograd.py
+++ thunder/transforms/torch_autograd.py
@@ -1,6 +1,9 @@
 from collections.abc import Callable, Sequence
+from dataclasses import replace
 from enum import auto, Enum
 from typing import Any
+
+from thunder.core.baseutils import check
 
 from thunder.core.prims import make_prim
 from thunder.core.proxies import TensorProxy
@@ -28,3 +31,37 @@ connect_to_torch_autograd = make_prim(
     "connect_to_torch_autograd",
     meta=torch_autograd_function_meta,
 )
+
+
+def get_backward(trace: TraceCtx) -> TraceCtx | Callable:
+    from thunder.executors.torchex import connect_to_autograd_impl
+
+    connect_to_autograd_bsym = next(
+        filter(
+            lambda bsym: bsym.sym.id in (connect_to_autograd_impl.id, connect_to_torch_autograd.id),
+            reversed(trace.bound_symbols),
+        ),
+        None,
+    )
+    check(connect_to_autograd_bsym is not None, lambda: "Could not find connect_to_autograd in the trace")
+    return connect_to_autograd_bsym.kwargs["backward"]
+
+
+def set_backward(trace: TraceCtx, backward: TraceCtx | Callable):
+    from thunder.executors.torchex import connect_to_autograd_impl
+
+    bsyms = list(trace.bound_symbols)
+    connect_to_autograd_bsym = next(
+        filter(
+            lambda bsym: bsym.sym.id in (connect_to_autograd_impl.id, connect_to_torch_autograd.id),
+            reversed(trace.bound_symbols),
+        ),
+        None,
+    )
+    check(connect_to_autograd_bsym is not None, lambda: "Could not find connect_to_autograd in the trace")
+    connect_to_autograd_bsym_idx = bsyms.index(connect_to_autograd_bsym)
+    bsyms[connect_to_autograd_bsym_idx] = replace(
+        connect_to_autograd_bsym, kwargs=connect_to_autograd_bsym.kwargs | {"backward": backward}
+    )
+    trace.bound_symbols = bsyms
+    return trace
