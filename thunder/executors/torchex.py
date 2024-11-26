f817a0be917bfb40098573ed2343d925e13bed00
Masaki Kozuki <mkozuki@nvidia.com>
2024-11-26 21:00:10 +0900

    register scaled_mm
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- thunder/executors/torchex.py
+++ thunder/executors/torchex.py
@@ -1403,11 +1403,31 @@ _register_implementation(prims.copy_with_setitem, copy_with_setitem_impl, checke
 #
 
 matmul = _register_torch_operation("matmul")
+_scaled_mm = _register_torch_operation("_scaled_mm")
 outer = _register_torch_operation("outer")
+
+
+def _scaled_mm_transform(
+    a: TensorLike,
+    b: TensorLike,
+    scale_a: TensorLike,
+    scale_b: TensorLike,
+    bias: TensorLike | None = None,
+    scale_result: TensorLike | None = None,
+    out_dtype: dtypeLike | None = None,
+    use_fast_accum: bool = False,
+):
+    result_dtype: torch.dtype = to_torch_dtype(a.dtype if out_dtype is None else out_dtype)
+    if b.stride()[0] != 1 and b.stride()[1] > 1:
+        b = b.t().contiguous().t()
+
+    print(f"{type(a)=}, {type(b)=}, {type(scale_a)=}, {type(scale_b)=}, {type(bias)=}, {type(scale_result)=}, {type(result_dtype)=}, {type(use_fast_accum)=}")
+    return _scaled_mm(a, b, scale_a, scale_b, bias, scale_result, result_dtype, use_fast_accum)
 
 _register_implementation(prims.matmul, matmul, checker=_always_executable)
 
 _register_implementation(ltorch.matmul, matmul, checker=_always_executable)
+_register_implementation(ltorch._scaled_mm, _scaled_mm, checker=_always_executable, execution_transform=_scaled_mm_transform)
 _register_implementation(ltorch.outer, outer, checker=_always_executable)
 
 #
