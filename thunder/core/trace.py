ff303cb84dc139b61cddf9ff7ac71a01a5e97b31
kshitij12345 <kshitijkalambarkar@gmail.com>
2024-11-26 13:54:07 +0100

    fix te and activation checkpointing for thunderFX

--- thunder/core/trace.py
+++ thunder/core/trace.py
@@ -126,10 +126,6 @@ class TraceCtx:
 
         self._any_future_tensors = False
 
-        # This is a detail for enabling transformer_engine's autocast manager.
-        # We only want the forward function to be called with ctx manager.
-        self._include_te_fp8_autocast = False
-
     @property
     def tags(self):
         return self._tags
@@ -422,9 +418,9 @@ class TraceCtx:
                 # NOTE: For TE v1.6 onwards, `fp8_autocast` checks if `torch.is_grad_enabled` for updating
                 # the FP8 scales/inverses. So this decorator should be applied before `torch.no_grad` (so that
                 # it is in grad enabled part).
-                from thunder.executors.transformer_engineex import _is_te_linear_enabled, _get_te_wrapper_string
+                from thunder.executors.transformer_engineex import _is_te_linear_fwd_present, _get_te_wrapper_string
 
-                if self._include_te_fp8_autocast and _is_te_linear_enabled(import_ctx, object_ctx):
+                if _is_te_linear_fwd_present(self.bound_symbols):
                     program.append(_get_te_wrapper_string())
 
                 # Disable gradients since Thunder takes care of this (for when calling torch operations)
@@ -520,8 +516,6 @@ def from_trace(trace: TraceCtx) -> TraceCtx:
     t.name_ctr = trace.name_ctr
     t.obj_name_ctr = trace.obj_name_ctr
     t.names = trace.names
-    # This is a detail for enabling transformer_engine's autocast manager.
-    t._include_te_fp8_autocast = trace._include_te_fp8_autocast
     t._tags = trace._tags.copy()
 
     t._siginfo = trace._siginfo
