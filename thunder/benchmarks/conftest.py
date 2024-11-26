b97ee886914db8fc165b2fd3eb82f92b83593866
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2024-11-26 16:04:47 +0200

    add benchmark isolation

--- thunder/benchmarks/conftest.py
+++ thunder/benchmarks/conftest.py
@@ -1,15 +1,111 @@
 import os
+from os import path
 import platform
 import psutil
 from typing import Any
 import warnings
 import importlib.util
+from pytest import hookimpl, TestReport, Item, Parser
+import multiprocessing as mp
+import subprocess
+
+BENCHMARK_JSON_DIR = "isolated_benchmarks_reports"
+FAILED_BENCHMARK_LOGS_DIR = "failed_benchmarks_logs"
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: Parser):
     # CLI option to specify where to store the benchmark results in asv format.
     # If not set or None, results won't be saved in asv.
     parser.addoption("--asv_bench_dir", action="store", default=os.getenv("THUNDER_BENCH_DIR"))
+
+    parser.addoption("--isolate-benchmarks", action="store_true", default=False)
+
+
+def launch_benchmark(target_file, target_name: str):
+    target_filename = target_name.replace("/", "_")
+
+    target_json = path.join(BENCHMARK_JSON_DIR, f"{target_filename}.json")
+
+    target_log = path.join(FAILED_BENCHMARK_LOGS_DIR, f"{target_filename}.log")
+    target_log_file = open(target_log, "w")
+
+    subprocess.run(
+        [
+            "pytest",
+            f"{target_file}::{target_name}",
+            "-vs",
+            "--benchmark-json",
+            target_json,
+        ],
+        check=True,
+        text=True,
+        stderr=subprocess.STDOUT,
+        stdout=target_log_file,
+    )
+
+
+def run_in_isolation(item: Item) -> TestReport:
+
+    process = mp.Process(
+        target=launch_benchmark,
+        args=(
+            item.location[0],
+            item.name,
+        ),
+    )
+    process.start()
+    process.join()
+
+    # Will mark skip as passed because pytest returns error only if there are failed tests.
+    outcome = "failed" if process.exitcode != 0 else "passed"
+    target_filename = item.name.replace("/", "_")
+
+    if outcome == "passed":
+        test_log = path.join(FAILED_BENCHMARK_LOGS_DIR, f"{target_filename}.log")
+        os.remove(test_log)
+
+    benchmark_json = path.join(BENCHMARK_JSON_DIR, f"{target_filename}.json")
+    if outcome == "failed" or path.getsize(benchmark_json) == 0:
+        os.remove(benchmark_json)
+
+    return TestReport(item.nodeid, item.location, keywords=item.keywords, outcome=outcome, longrepr=None, when="call")
+
+
+@hookimpl(tryfirst=True)
+def pytest_runtest_protocol(item: Item, nextitem: Item):
+    # If the option was not passed, let pytest manage the run.
+    if not item.config.getoption("--isolate-benchmarks"):
+        return None
+
+    ihook = item.ihook
+    ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
+    test_report = run_in_isolation(item)
+
+    ihook.pytest_runtest_logreport(report=test_report)
+    ihook.pytest_runtest_logfinish(nodeid=item.nodeid, location=item.location)
+    return True
+
+
+def pytest_runtestloop(session):
+    # Can it be saved as part of the config to avoid the global?
+    global BENCHMARK_JSON_DIR, FAILED_BENCHMARK_LOGS_DIR
+
+    if not session.config.getoption("--isolate-benchmarks"):
+        return None
+
+    mp.set_start_method("spawn")
+
+    from _pytest.terminal import TerminalReporter
+    terminal: TerminalReporter = session.config.pluginmanager.get_plugin("terminalreporter")
+
+    custom_report_dir = os.getenv("THUNDER_BENCH_DIR")
+    BENCHMARK_JSON_DIR = custom_report_dir if custom_report_dir else BENCHMARK_JSON_DIR
+
+    os.makedirs(BENCHMARK_JSON_DIR, exist_ok=True)
+    os.makedirs(FAILED_BENCHMARK_LOGS_DIR, exist_ok=True)
+
+    terminal.write_line(f"Saving failed benchmarks logs in {FAILED_BENCHMARK_LOGS_DIR}/")
+    terminal.write_line(f"Saving benchmarks reports in {BENCHMARK_JSON_DIR}/")
 
 
 def pytest_sessionfinish(session, exitstatus):
