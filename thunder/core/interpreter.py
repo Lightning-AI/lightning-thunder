fd1071b1e9ba4f3a3455a49a3f209bce0c5fbe43
jjsjann123 <jiej@nvidia.com>
2024-12-01 14:09:00 -0800

    removing unused import

--- thunder/core/interpreter.py
+++ thunder/core/interpreter.py
@@ -284,8 +284,6 @@ def wrap(value: Any, /, *, provenance: ProvenanceRecord) -> WrappedValue:
 
 
 def wrap_const(value: Any, /, *, provenance: ProvenanceRecord | None = None) -> WrappedValue:
-    from thunder.core.proxies import Proxy
-
     if provenance is None:
         provenance = ProvenanceRecord(inst=PseudoInst.CONSTANT, inputs=[], value=value)
     return wrap(value, provenance=provenance)
