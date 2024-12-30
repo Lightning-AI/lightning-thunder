2204893b8b15bea103d4b3672d48e20fd30b0549
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2024-12-30 11:00:22 +0200

    Make TensorProxy requires_grad argument to be False by default

--- thunder/tests/test_core.py
+++ thunder/tests/test_core.py
@@ -3115,6 +3115,21 @@ def test_debug_options():
     assert dill.dumps(dict(DebugOptions.__dict__)) == initial_state
 
 
+def test_default_tensor_proxy():
+    from thunder.core.proxies import TensorProxy
+    from thunder.core.trace import detached_trace
+    from thunder.core.dtypes import float32
+    from thunder.core.devices import cpu
+
+    # It should be possible to create a TensorProxy with default values for all
+    # optional arguments
+    with detached_trace():
+        t = TensorProxy(shape=(1,), device=cpu, dtype=float32)
+    assert not t.requires_grad
+    assert t.device == cpu
+    assert t.dtype == float32
+
+
 def test_proxy_same_name():
     from thunder.core.proxies import TensorProxy
     from thunder.core.trace import detached_trace
@@ -3123,5 +3138,5 @@ def test_proxy_same_name():
 
     with detached_trace():
         for _ in range(2):
-            t = TensorProxy(name="test", shape=(1,), device=cpu, dtype=float32, requires_grad=True)
+            t = TensorProxy(name="test", shape=(1,), device=cpu, dtype=float32)
             assert t.name == "test"
