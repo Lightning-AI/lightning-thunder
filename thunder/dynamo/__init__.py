329c7e1af551a89f208ea2d4956973d44e986e0e
Tom Fogal <60981+tfogal@users.noreply.github.com>
2025-01-07 11:35:13 -0800

    Add a new dynamo.last_traces entry point.
    To make it simple to grab the important traces.

--- thunder/dynamo/__init__.py
+++ thunder/dynamo/__init__.py
@@ -1,4 +1,4 @@
 from thunder.dynamo.compiler import ThunderCompiler, thunderfx
+from thunder.dynamo.utils import last_traces
 
-
-__all__ = ["ThunderCompiler", "thunderfx"]
+__all__ = ["ThunderCompiler", "thunderfx", "last_traces"]
