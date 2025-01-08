0cfacfc4f3131e3a0488ecd3f327b3a0300573ac
Tom Fogal <60981+tfogal@users.noreply.github.com>
2025-01-08 10:44:40 -0800

    oops, bugfix somehow not caught earlier.

--- thunder/dynamo/__init__.py
+++ thunder/dynamo/__init__.py
@@ -1,4 +1,3 @@
 from thunder.dynamo.compiler import ThunderCompiler, thunderfx
-from thunder.dynamo.utils import last_traces
 
-__all__ = ["ThunderCompiler", "thunderfx", "last_traces"]
+__all__ = ["ThunderCompiler", "thunderfx"]
