db7ddc61cf05e90ce50c3cf9f6bb86a5ab294093
Thomas Viehmann <tv.code@beamnet.de>
2025-01-08 16:31:56 +0100

    skip nvfuser-breaking test

--- thunder/tests/test_networks.py
+++ thunder/tests/test_networks.py
@@ -419,12 +419,13 @@ def test_thunderfx_mistral_nemo_small():
     assert mdl._backend.subgraph_infos, "Should have at least 1 subgraph"
 
 
+# disabled "Qwen/Qwen2.5-7B-Instruct" see https://github.com/NVIDIA/Fuser/issues/3682
 @pytest.mark.skipif(
     version_between(torch.__version__, min_ver="2.6.0dev0", max_ver="2.6.0a99"),
     reason="https://github.com/Lightning-AI/lightning-thunder/issues/1471",
 )
 @thunder.tests.framework.requiresCUDA
-@pytest.mark.parametrize("model_id", ["Qwen/Qwen2.5-7B-Instruct", "microsoft/Phi-3-mini-128k-instruct"])
+@pytest.mark.parametrize("model_id", ["microsoft/Phi-3-mini-128k-instruct"])
 def test_hf_for_nemo(model_id):
     from thunder.dynamo import thunderfx
     from transformers import AutoConfig, AutoModelForCausalLM
