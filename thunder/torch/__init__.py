71e0f91ad03426b57cf6a9a233f62394ab1828fa
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2025-02-07 12:57:34 +0200

    multiply weight in the input dtype

--- thunder/torch/__init__.py
+++ thunder/torch/__init__.py
@@ -3768,7 +3768,6 @@ def rms_norm(
     weight: None | TensorLike = None,
     eps: None | float = None,
 ):
-    input_dtype = a.dtype
     a = clang.maybe_convert_to_dtype(a, thunder.float32, enforce_safe_casting=True)
     if eps is None:
         eps = torch.finfo(to_torch_dtype(a.dtype)).eps
@@ -3776,9 +3775,11 @@ def rms_norm(
     reduction_dims = _check_normalized_shape_and_get_reduction_dims(a, normalized_shape, weight)
     norm_a = mean(a * a, dim=reduction_dims, keepdim=True, dtype=None)
     a_normed = a * rsqrt(norm_a + eps)
+
+    a_normed = clang.maybe_convert_to_dtype(a_normed, weight.dtype, enforce_safe_casting=True)
     if weight is not None:
         a_normed = a_normed * weight
-    return clang.maybe_convert_to_dtype(a_normed, input_dtype, enforce_safe_casting=True)
+    return a_normed
 
 
 if hasattr(torch.nn.functional, "rms_norm"):
