77eff9007ca3bde4c4013cbdca335e460a20a432
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2025-03-03 13:27:40 +0200

    make te state management visible in the trace

--- thunder/executors/fun_te_ex.py
+++ thunder/executors/fun_te_ex.py
@@ -1,18 +1,27 @@
-import transformer_engine.pytorch as te
-from transformer_engine.pytorch.ops import BasicLinear
-from transformer_engine.pytorch.fp8 import get_fp8_max, _amax_and_scale_update
+from dataclasses import dataclass, field
 
+from thunder.core.prims import OpTags
 from thunder.core.proxies import AnyProxy, TensorProxy
 from thunder.core.transforms import get_grad, put_grads
-
 from thunder.extend import OperatorExecutor, register_executor
-
 import thunder.torch as ltorch
+
+import transformer_engine.pytorch as te
+from transformer_engine.pytorch.ops import BasicLinear
+from transformer_engine.pytorch.fp8 import RecipeState, get_fp8_max, _amax_and_scale_update
 
 functional_te_ex = OperatorExecutor("functional_te")
 register_executor(functional_te_ex)
 
-recipe = te.fp8.get_default_fp8_recipe()
+
+@dataclass
+class FP8States:
+    recipe = None
+    forward_state_buffer: list[RecipeState] = field(default_factory=list)
+    backward_state_buffer: list[RecipeState] = field(default_factory=list)
+
+
+fun_te_states = FP8States()
 
 
 def _functional_te_checker(a, w, bias):
@@ -23,11 +32,25 @@ def _functional_te_checker(a, w, bias):
     return True
 
 
-def _linear_fwd_meta(a, w, bias, forward_recipe_state, input_quantizer, weight_quantizer):
+def _te_fp8_recipe_meta():
+    return AnyProxy(object())
+
+
+def _te_fp8_recipe_impl():
+    if not fun_te_states.recipe:
+        fun_te_states.recipe = te.fp8.get_default_fp8_recipe()
+
+    return fun_te_states.recipe
+
+
+_te_fp8_recipe = functional_te_ex.register_operator("te_fp8_recipe", meta=_te_fp8_recipe_meta, fn=_te_fp8_recipe_impl)
+
+
+def _linear_fwd_meta(a: TensorProxy, w, bias, forward_recipe_state, input_quantizer, weight_quantizer):
     # TODO if not requires_grad -> return return TensorProxy(like=a), None, None
-    # Placeholder object
-    o = object()
-    return TensorProxy(like=a), TensorProxy(like=a), TensorProxy(like=a)
+    if a.requires_grad:
+        return TensorProxy(like=a), TensorProxy(like=a), TensorProxy(like=a)
+    return TensorProxy(like=a), None, None
 
 
 def _linear_fwd_impl(a, w, bias, forward_recipe_state, input_quantizer, weight_quantizer):
@@ -39,10 +62,7 @@ def _linear_fwd_impl(a, w, bias, forward_recipe_state, input_quantizer, weight_q
         input_quantizer=input_quantizer,
         weight_quantizer=weight_quantizer,
     )
-    # Needs to be in the impl because otherwise it could be ran out of order.
-    _amax_and_scale_update(
-        forward_recipe_state.amax_history, forward_recipe_state.scale, get_fp8_max(recipe, "forward"), recipe
-    )
+
     return out, gemm_a, gemm_w
 
 
@@ -50,8 +70,15 @@ _te_linear_fwd = functional_te_ex.register_operator("te_functional_fwd", meta=_l
 
 
 def _te_linear_fwd_wrapper(a, w, bias):
+    recipe = _te_fp8_recipe()
+
     forward_recipe_state, input_quantizer, weight_quantizer = _te_fp8_state(recipe, ("forward",), 2)
+    fun_te_states.forward_state_buffer += [forward_recipe_state]
+
     out, _, _ = _te_linear_fwd(a, w, bias, forward_recipe_state, input_quantizer, weight_quantizer)
+
+    _te_fp8_syncronization(recipe, True)
+
     return out
 
 
@@ -70,9 +97,7 @@ def _linear_bwd_impl(grad_o, a, w, input_quantizer, weight_quantizer, backward_r
         weight_quantizer=weight_quantizer,
         grad_output_quantizer=grad_output_quantizer,
     )
-    _amax_and_scale_update(
-        backward_recipe_state.amax_history, backward_recipe_state.scale, get_fp8_max(recipe, "backward"), recipe
-    )
+
     return grad_input, grad_weight
 
 
@@ -96,12 +121,32 @@ def _te_fp8_state_impl(recipe, mode, num_quantizers):
 _te_fp8_state = functional_te_ex.register_operator("te_fp8_state", meta=_te_fp8_state_meta, fn=_te_fp8_state_impl)
 
 
+# create every time and then resort to cse to eliminate all of them but the last one.
+def _te_fp8_sync_meta(recipe: RecipeState, forward: bool):
+    return None
+
+
+def _te_fp8_sync_impl(recipe: RecipeState, forward: bool):
+    state_buffer = fun_te_states.forward_state_buffer if forward else fun_te_states.backward_state_buffer
+    for state in state_buffer:
+        _amax_and_scale_update(state.amax_history, state.scale, get_fp8_max(recipe, forward), fun_te_states.recipe)
+    return None
+
+
+_te_fp8_syncronization = functional_te_ex.register_operator(
+    "te_fp8_sync", meta=_te_fp8_sync_meta, fn=_te_fp8_sync_impl, tags=(OpTags.DONT_DCE, OpTags.CSE_KEEP_LAST)
+)
+
+
 def _te_linear_bwd_wrapper(a, w, bias):
+    recipe = _te_fp8_recipe()
+
     forward_recipe_state, input_quantizer, weight_quantizer = _te_fp8_state(recipe, ("forward",), 2)
 
     primal, gemm_a, gemm_w = _te_linear_fwd(a, w, bias, forward_recipe_state, input_quantizer, weight_quantizer)
 
     backward_recipe_state, grad_output_quantizer = _te_fp8_state(recipe, ("backward",), 1)
+    fun_te_states.backward_state_buffer += [backward_recipe_state]
 
     grad_a, grad_w = _te_linear_bwd(
         get_grad(primal),
@@ -112,6 +157,9 @@ def _te_linear_bwd_wrapper(a, w, bias):
         backward_recipe_state,
         grad_output_quantizer,
     )
+
+    _te_fp8_syncronization(recipe, False)
+
     put_grads((a, w), (grad_a, grad_w))
     return primal
 
