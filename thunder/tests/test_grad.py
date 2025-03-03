374cbed597dd5ef5eb83092caa8199c1e5d354ba
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2025-03-03 09:55:54 +0200

    Revert "Xfail test_requires_grad"
    This reverts commit 2b3828bbf7811de4107935c34632c24c49d8a8ae.

--- thunder/tests/test_grad.py
+++ thunder/tests/test_grad.py
@@ -715,9 +715,6 @@ def test_vjp_correctness_einsum_manual(op, device, dtype, executor, comp):
 # TODO Extend requires_grad so that tensors produced from thunder.jit functions requires_grad
 #   and have their autograd functions set properly
 # Tests that we track the requires_grad property properly
-# TODO Reenable this test when we track the requires_grad consistently for all operators
-# See https://github.com/Lightning-AI/lightning-thunder/issues/1768
-@pytest.mark.xfail(strict=True, reason="Requires_grad propagation is not implemented")
 @instantiate(dtypes=(dtypes.float32,))
 def test_requires_grad(executor, device, dtype):
     import thunder.torch as ltorch
