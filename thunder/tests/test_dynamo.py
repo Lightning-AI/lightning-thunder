35e12e53ddd21c1fe5d76df0817df6c6b364317b
Yan Wang <kiya00wy@gmail.com>
2025-03-11 10:46:58 +0000

    adds test

--- thunder/tests/test_dynamo.py
+++ thunder/tests/test_dynamo.py
@@ -1445,3 +1445,39 @@ def test_save_failing_repros(tmp_path):
     with patch("thunder.dynamo.report.FXGraphReport.run_repro", side_effect=Exception("run_Repro raises exception")):
         save_failing_repros(thunder_fxgraph_reports[0].subgraph_reports, ThunderCompileSpecification(), tmp_path)
     assert os.path.exists(tmp_path / "graph0_thunder_0.py")
+
+
+def test_autograd_function_fx_report(tmp_path):
+    class Sin(torch.autograd.Function):
+        @staticmethod
+        def forward(ctx, x):
+            ctx.save_for_backward(x)
+            return torch.sin(x)
+
+        @staticmethod
+        def backward(ctx, g):
+            (x,) = ctx.saved_tensors
+            return g * torch.cos(x) * 100
+
+    def func(x):
+        return torch.cos(x) + Sin.apply(x)
+
+    x = torch.ones(2, 2, device="cuda", requires_grad=True)
+    results = fx_report(func, x)
+
+    assert len(results.fx_graph_reports) == 1  # 1 Dynamo graph
+    fx_graph_report = results.fx_graph_reports[0]
+    thunder_fx_graph_report = analyze_thunder_splits(fx_graph_report)
+    assert len(thunder_fx_graph_report.subgraph_reports) == 1  # no split
+    thunder_split_report = thunder_fx_graph_report.subgraph_reports[0]
+
+    thunder_split_report.run_repro(ThunderCompileSpecification())
+    thunder_split_report.run_benchmark(ThunderCompileSpecification(), WallTime)
+    thunder_split_report.write_benchmark(tmp_path, ThunderCompileSpecification(), WallTime)
+    thunder_split_report.write_repro(tmp_path, ThunderCompileSpecification(), file_name="repro.py")
+
+    cmd = [sys.executable]
+    py_files = list(tmp_path.rglob("*.py"))
+    assert len(py_files) == 2
+    for file in py_files:
+        run_script(file, cmd)
