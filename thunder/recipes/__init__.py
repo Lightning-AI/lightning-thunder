a2def4af2ed4e843ee1465307ca56a47ac7a84fd
Luca Antiga <luca@lightning.ai>
2025-03-14 18:45:48 +0100

    Keep auto reserved as a recipe name

--- thunder/recipes/__init__.py
+++ thunder/recipes/__init__.py
@@ -15,4 +15,6 @@ def get_recipe_names():
     return list(names_to_recipes.keys())
 
 def register_recipe(name, cls):
+    if name == "auto":
+        raise ValueError("Recipe name 'auto' is reserved.")
     names_to_recipes[name] = cls
