ff72d98b0d62184fa7f18cad05e6b2d770628fe4
Thomas Viehmann <tv@beamnet.de>
2025-04-28 11:43:44 +0200

    debug option check traces

--- thunder/tests/test_check_trace.py
+++ thunder/tests/test_check_trace.py
@@ -24,3 +24,35 @@ def test_missing_symbol():
 
     with pytest.raises(AssertionError, match="unknown proxy"):
         check_trace(tr)
+
+
+def test_debug_option():
+    class BrokenTransform(thunder.core.transform_common.Transform):
+        def transform_traces_pre_prologue(self, pro, comp, epi, **kwargs):
+            new_comp = thunder.core.trace.from_trace(comp)
+            new_comp.bound_symbols = comp.bound_symbols[:]
+            del new_comp.bound_symbols[2]
+            return pro, new_comp, epi
+
+    def fn(a, b):
+        c = a + b
+        d = 2 * c
+        return d
+
+    a = torch.randn(2, 2)
+    b = torch.randn(2, 2)
+
+    # works
+    jfn = thunder.jit(fn, debug_options=thunder.DebugOptions(check_traces=True))
+    jfn(a, b)
+
+    # broken with nice error
+    jfn = thunder.jit(fn, transforms=(BrokenTransform(),), debug_options=thunder.DebugOptions(check_traces=True))
+
+    with pytest.raises(AssertionError, match="unknown proxy"):
+        jfn(a, b)
+
+    # broken with less nice error
+    jfn = thunder.jit(fn, transforms=(BrokenTransform(),))
+    with pytest.raises(UnboundLocalError, match="cannot access local"):
+        jfn(a, b)
