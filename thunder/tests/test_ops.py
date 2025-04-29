04685c1b7d0611b2141ce2966489841d1b03d341
KaelanDt <kaelan@lightning.ai>
2025-04-29 14:20:34 +0000

    remove stray print

--- thunder/tests/test_ops.py
+++ thunder/tests/test_ops.py
@@ -35,7 +35,6 @@ def test_errors(op, device, dtype, executor, comp):
 # TODO: should snippets be able to access the original opinfo? -- No?
 # TODO: revisit atol/rtol, maybe be more selective about which ops need a more permissive check
 def snippet_torch_consistency(op: OpInfo, torch_op, sample: SampleInput, comp: Callable):
-    print(op, sample)
     thunder_result = op(*sample.args, **sample.kwargs)
     torch_result = torch_op(*sample.args, **sample.kwargs)
 
