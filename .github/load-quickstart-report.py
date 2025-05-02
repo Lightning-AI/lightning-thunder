38816587fb88330a247f4c44ba595a9c7b5332ea
Jirka B <j.borovec+github@gmail.com>
2025-05-02 17:01:04 +0200

    load report

---
+++ .github/load-quickstart-report.py
@@ -0,0 +1,25 @@
+import json
+import sys
+from datetime import datetime
+import glob
+import os.path
+
+from lightning_sdk import Studio, Job, Machine, Status
+
+
+def main(report_path: str = "quickstart_report.json"):
+    """Load and print the quickstart report."""
+    with open(report_path, "r") as fp:
+        report = json.load(fp)
+
+    print("Quickstart report:")
+    for name, status in report.items():
+        status_icon = "✅" if status == "completed" else "❌"
+        print(f"{status_icon}: {name}")
+
+
+if __name__ == "__main__":
+    # optional path to the report file
+    system_args = sys.argv[1:]
+    main_args = {"report_path": system_args[0]} if len(system_args) > 0 else {}
+    main(**main_args)
