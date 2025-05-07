af95c9065e94e9cca7d11bb7f99f92b4000d36df
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2025-05-07 13:38:15 +0300

    add stateful executor doc string

--- thunder/extend/__init__.py
+++ thunder/extend/__init__.py
@@ -275,27 +275,71 @@ class OperatorExecutor(Executor):
 
 
 class StatefulExecutor(OperatorExecutor):
-    def __init__(self, name, *, version=None):
-        super().__init__(name, version=version)
-        self.state_count: int = 0
+    """
+    A specialized executor for operators that need to keep a state for as long as the trace lifetime.
 
-    def register_stateful_operator(self, base_name: str, state_class, *, meta):
-        def register_state(*args, **kwargs):
-            state_id = self.state_count
-            name = f"{base_name}_{state_id}"
+    This executor is able to register operators and bind them to their state class.
+    It uses a counter to keep operator names unique.
+
+    Key Features:
+        - Creates and binds state object to operators.
+        - Gives unique naming to the operators by using a counter.
+        - Disables cacheing of it's symbols' `grad_transform`.
+
+    Example:
+        >>> executor = StatefulExecutor()
+        >>> op = executor.register_operator(
+        ...     name="temp_add",
+        ...     StateClass,
+        ...     [...same arguments that would have been passed to register_operator...]
+        ... )
+
+    For more examples check out transformer engine executor.
+    """
+
+    def __init__(self, name: Hashable, *, version: Any | None = None):
+        super().__init__(name, version=version)
+        self.counter: int = 0
+
+    def register_stateful_operator(self, name: str, state_class, *, meta) -> Callable[..., Any]:
+        """
+        Sets up a function that creates and registers new symbol for the given operator name and state class.
+        The output functiona also binds an instance of the state class to the bound symbol call context.
+
+        Args:
+            name: The name of the operator. Note that it is going to be combined with the counter number to generate a unique name.
+            state_class: The name of a user defined class that implements a __call__ method that returns the
+                operator output (i.e. the value of the state).
+            meta: The meta function for the operator.
+
+        Returns:
+            out: Instance of `create_new_symbol_and_call` function that register the operator and binds the state object upon call.
+        """
+
+        def create_new_symbol_and_call(*args, **kwargs):
+            op_name = f"{name}_{self.counter}"
 
             def bind_state(bsym):
-                bsym._call_ctx = {name: state_class()}
+                bsym._call_ctx = {op_name: state_class()}
 
-            sym = self.register_operator(name, meta=meta, bind_postprocess=bind_state)
+            sym: Symbol = self.register_operator(op_name, meta=meta, bind_postprocess=bind_state)
 
-            self.state_count += 1
+            self.counter += 1
             return sym(*args, **kwargs)
 
-        return register_state
+        return create_new_symbol_and_call
 
     def get_grad_transform(self, sym: Symbol):
-        grad_transform = super().get_grad_transform(sym)
+        """
+        Since every call to the symbol is unique, this function disables cacheing on the symbol's grad transform.
+
+        Args:
+            sym: Symbol queried for grad transform.
+
+        Returns:
+            out: sym's grad_transform wrapped in `disable_caching_split_forward_and_backward` decorator or None.
+        """
+        grad_transform: Any | None = super().get_grad_transform(sym)
         if grad_transform:
             # Always disable cache for stateful grad transform
             return disable_caching_split_forward_and_backward(grad_transform)
