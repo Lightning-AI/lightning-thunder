cbfc9b7d224865d614604f69c8060f51013ab780
Ali Al-shaarawy <ali@lightning.ai>
2025-05-07 12:20:38 +0000

    make check_trace errors better

--- thunder/dev_utils/check_trace.py
+++ thunder/dev_utils/check_trace.py
@@ -23,10 +23,10 @@ def check_trace(trace):
     known_proxies = {}
     for bsym in trace.bound_symbols:
         for a in bsym.flat_proxy_args:
-            assert a.name in known_proxies, f"unknown proxy {a.name} is used in {bsym}"
-            assert known_proxies[a.name] is a, f"proxy name collision {a.name} in {bsym}"
+            assert a.name in known_proxies, f"unknown proxy {a.name} is used in {bsym} args"
+            assert known_proxies[a.name] is a, f"proxy name collision {a.name} in {bsym} args"
         for o in bsym.flat_proxy_outs:
-            assert known_proxies.get(o.name, o) is o, f"proxy name collision {a.name} in {bsym}"
+            assert known_proxies.get(o.name, o) is o, f"proxy name collision {o.name} in {bsym} outputs"
             known_proxies[o.name] = o
         check_subsymbols(bsym)
 
