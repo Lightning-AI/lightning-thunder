2fce3e428781918367f419545eaab4c61319ad89
KaelanDt <kaelan@lightning.ai>
2025-05-12 16:09:33 +0000

    add semi-working benchmarking script for HF model

--- examples/benchmarks/hf_benchmarks.py
+++ examples/benchmarks/hf_benchmarks.py
@@ -15,6 +15,7 @@ class DebugRecipe(BaseRecipe):
         config = super().setup_config()
         config["skip_inplace_functionalization"] = True
         config["skip_alias_functionalization"] = False
+        config["disable_inplace_copy_check"] = True
         return config
 
 
@@ -22,211 +23,105 @@ device = "cuda"
 model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
 root = Path("./results")
-
-save_traces = False
+save_traces = True
 save_profiles = False
-plugins = None #  "reduce-overhead"
-
-plugins_str = "" if plugins is None else plugins if type(plugins) == str else "-".join(plugins)
+plugins = None  # or "reduce-overhead"
+plugins_str = "" if plugins is None else plugins if isinstance(plugins, str) else "-".join(plugins)
 
 recipe = DebugRecipe()
 
-
-def inference_gen():
-    print("Inference gen")
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
-
-    with torch.device(device):
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.bfloat16
-        )
-        model.requires_grad_(False)
-        model.eval()
-
-        inp = tokenizer(["Hello world! Here's a long story"], return_tensors='pt')
-
-    def generate(model, inp, cache=None):
-        _ = model.generate(**inp, 
-            pad_token_id=tokenizer.eos_token_id,
-            do_sample=False,
-            cache_implementation=cache,
-            max_new_tokens=100,
-            top_p=1.0
-        )
-
-    print("  running PyTorch eager")
-    eager_time = benchmark_n(10, generate, model, inp)
+def run_and_profile(tag, fn, model, inp, trace_key, cache=None):
+    print(f"  running PyTorch eager for {tag}")
+    eager_time = benchmark_n(10, fn, model, inp)
 
     thunder_model = thunder.compile(model, plugins=plugins, recipe=recipe)
+    print(f"  running Thunder for {tag}")
+    thunder_time = benchmark_n(10, fn, thunder_model, inp, cache=cache)
 
-    if save_profiles:
-        with profile(activities=[ProfilerActivity.CUDA]) as thunder_prof:
-            for _ in range(10):
-                with record_function("generate"):
-                    _ = generate(thunder_model, inp, cache='static')
-
-        with open(root / f"inference_gen_profile_{plugins_str}.txt", "w") as f:
-            table = thunder_prof.key_averages().table(sort_by="cpu_time_total")
-            f.write(table)
-
-    if save_traces:
-        trace = thunder.last_traces(thunder_model)[-1]
-        with open(root / f"inference_gen_trace_{plugins_str}.py", "w") as f:
-            f.write(str(trace))
-
-    print("  running Thunder")
-    thunder_time = benchmark_n(10, generate, thunder_model, inp, cache='static')
-
-    with open(root / f"inference_gen_timings_{plugins_str}.txt", "w") as f:
+    # Save timings
+    with open(root / f"{tag}_timings_{plugins_str}.txt", "w") as f:
         f.write(f"Eager: {eager_time:.2f}ms\n")
         f.write(f"Thunder: {thunder_time:.2f}ms")
 
-
-def inference_fwd():
-    print("Inference fwd")
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
-
-    with torch.device(device):
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.bfloat16
-        )
-        model.requires_grad_(False)
-        model.eval()
-
-        inp = tokenizer(
-            ["Hello world! Here's a long story"],
-            return_tensors='pt', max_length=2048, truncation=True
-        )
-
-    def fwd(model, inp):
-        _ = model(**inp)
-
-    print("  running PyTorch eager")
-    eager_time = benchmark_n(10, fwd, model, inp)
-
-    thunder_model = thunder.compile(model.model, plugins=plugins, recipe=recipe)
-
+    # Save trace and run once 
     if save_traces:
+        if fn.__name__ == "generate":
+            # need to run the model once to get the trace
+            thunder_model(**inp)
+        else:
+
+            _ = fn(thunder_model, inp, cache=cache)
         trace = thunder.last_traces(thunder_model)[-1]
-        with open(root / f"inference_fwd_trace_{plugins_str}.py", "w") as f:
+        with open(root / f"{tag}_trace_{plugins_str}.py", "w") as f:
             f.write(str(trace))
 
     if save_profiles:
         with profile(activities=[ProfilerActivity.CUDA]) as thunder_prof:
             for _ in range(10):
-                with record_function("generate"):
-                    _ = fwd(thunder_model, inp)
-
-        with open(root / f"inference_fwd_profile_{plugins_str}.txt", "w") as f:
-            table = thunder_prof.key_averages().table(sort_by="cpu_time_total")
-            f.write(table)
-
-    print("  running Thunder")
-    thunder_time = benchmark_n(10, fwd, thunder_model, inp)
-
-    with open(root / f"inference_fwd_timings_{plugins_str}.txt", "w") as f:
-        f.write(f"Eager: {eager_time:.2f}ms\n")
-        f.write(f"Thunder: {thunder_time:.2f}ms")
-
-
-def training_fwd():
-    print("Training fwd")
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
-
-    with torch.device(device):
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.bfloat16
-        )
-
-        inp = tokenizer(
-            ["Hello world! Here's a long story"],
-            return_tensors='pt', max_length=2048, truncation=True
-        )
-        inp["labels"] = inp["input_ids"]
-
-    def fwd(model, inp):
-        _ = model(**inp)
-
-    print("  running PyTorch eager")
-    eager_time = benchmark_n(10, fwd, model, inp)
-
-    thunder_model = thunder.compile(model.model, plugins=plugins, recipe=recipe)
-
-    if save_traces:
-        trace = thunder.last_traces(thunder_model)[-1]
-        with open(root / f"training_fwd_trace_{plugins_str}.py", "w") as f:
-            f.write(str(trace))
-
-    if save_profiles:
-        with profile(activities=[ProfilerActivity.CUDA]) as thunder_prof:
-            for _ in range(10):
-                with record_function("fwd"):
-                    _ = fwd(thunder_model, inp)
+                with record_function(fn.__name__):
+                    _ = fn(thunder_model, inp)
+            
+            # thunder_prof.export_chrome_trace(str(root / f"{tag}_thunder_trace_{plugins_str}.json"))
 
         with open(root / f"training_fwd_profile_{plugins_str}.txt", "w") as f:
             table = thunder_prof.key_averages().table(sort_by="cpu_time_total")
             f.write(table)
+            
+def inference_gen():
+    print("Inference gen")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+    model.eval().requires_grad_(False).to(device)
+    inp = tokenizer(["Hello world! Here's a long story"], return_tensors='pt').to(device)
 
-    print("  running Thunder")
-    thunder_time = benchmark_n(10, fwd, thunder_model, inp)
+    def generate(model, inp, cache=None):
+        model.generate(**inp, pad_token_id=tokenizer.eos_token_id, do_sample=False,
+                       cache_implementation=cache, max_new_tokens=100, top_p=1.0)
 
-    with open(root / f"training_fwd_timings_{plugins_str}.txt", "w") as f:
-        f.write(f"Eager: {eager_time:.2f}ms\n")
-        f.write(f"Thunder: {thunder_time:.2f}ms")
+    run_and_profile("inference_gen", generate, model, inp, "generate", cache='static')
 
+def inference_fwd():
+    print("Inference fwd")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16)
+    model.eval().requires_grad_(False).to(device)
+    inp = tokenizer(["Hello world! Here's a long story"], return_tensors='pt', max_length=2048, truncation=True).to(device)
+
+    def fwd(model, inp, cache=None):
+        model(**inp)
+
+    run_and_profile("inference_fwd", fwd, model, inp, "fwd")
+
+def training_fwd():
+    print("Training fwd")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16).to(device)
+    inp = tokenizer(["Hello world! Here's a long story"], return_tensors='pt', max_length=2048, truncation=True)
+    inp["labels"] = inp["input_ids"]
+    inp = inp.to(device)
+
+    def fwd(model, inp, cache=None):
+        model(**inp)
+
+    run_and_profile("training_fwd", fwd, model, inp, "fwd")
 
 def training_fwd_bwd():
     print("Training fwd_bwd")
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+    model = transformers.AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16).to(device)
+    inp = tokenizer(["Hello world! Here's a long story"], return_tensors='pt', max_length=2048, truncation=True)
+    inp["labels"] = inp["input_ids"]
+    inp = inp.to(device)
 
-    with torch.device(device):
-        model = transformers.AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.bfloat16
-        )
-
-        inp = tokenizer(
-            ["Hello world! Here's a long story"],
-            return_tensors='pt', max_length=2048, truncation=True
-        )
-        inp["labels"] = inp["input_ids"]
-
-    print(inp.keys())
-
-    def fwd_bwd(model, inp):
+    def fwd_bwd(model, inp, cache=None):
         loss = model(**inp).loss
         loss.backward()
 
-    print("  running PyTorch eager")
-    eager_time = benchmark_n(10, fwd_bwd, model, inp)
-
-    thunder_model = thunder.compile(model.model, plugins=plugins, recipe=recipe)
-
-    if save_traces:
-        trace = thunder.last_traces(thunder_model)[-1]
-        with open(root / f"training_fwd_bwd_trace_{plugins_str}.py", "w") as f:
-            f.write(str(trace))
-
-    if save_profiles:
-        with profile(activities=[ProfilerActivity.CUDA]) as thunder_prof:
-            for _ in range(10):
-                with record_function("fwd_bwd"):
-                    _ = fwd_bwd(thunder_model, inp)
-
-        with open(root / f"training_fwd_bwd_profile_{plugins_str}.txt", "w") as f:
-            table = thunder_prof.key_averages().table(sort_by="cpu_time_total")
-            f.write(table)
-
-    print("  running Thunder")
-    thunder_time = benchmark_n(10, fwd_bwd, thunder_model, inp)
-
-    with open(root / f"training_fwd_bwd_timings_{plugins_str}.txt", "w") as f:
-        f.write(f"Eager: {eager_time:.2f}ms\n")
-        f.write(f"Thunder: {thunder_time:.2f}ms")
+    run_and_profile("training_fwd_bwd", fwd_bwd, model, inp, "fwd_bwd")
 
 
 if __name__ == "__main__":
     torch.backends.cuda.matmul.allow_tf32 = True
-
     root.mkdir(parents=True, exist_ok=True)
 
     inference_gen()
