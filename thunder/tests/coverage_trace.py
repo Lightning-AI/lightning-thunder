d17d84d9d899bce6b070aa8579c5b46a41c72311
tejapulagam <tejapulagam@gmail.com>
2025-05-21 20:18:55 -1000

    changed test file name

---
+++ thunder/tests/coverage_trace.py
@@ -0,0 +1,96 @@
+import torch
+import traceback
+import thunder 
+
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    AutoModel,
+    AutoModelForCausalLM, 
+    AutoModelForSeq2SeqLM,
+    AutoModelForImageClassification
+)
+
+
+MODEL_LIST = [
+    "gpt2",
+    "bert-base-uncased",
+    "google/reformer-enwik8",
+    "facebook/bart-large",
+    "t5-base",
+    "xlnet-base-cased",
+    "facebook/dinov2-small",
+    "albert-base-v2",
+    "google/electra-base-discriminator",
+    "facebook/opt-1.3b", 
+    "google/vit-base-patch16-224"
+ ]
+
+MODEL_TASKS = {
+    "openai/clip-vit-large-patch14": "clip",
+    "facebook/dinov2-small": "vision"
+}
+
+def get_model_class(model_name, config):
+    task = MODEL_TASKS.get(model_name, "text")
+    if task == "vision":
+        return AutoModelForImageClassification
+    elif config.architectures and "CausalLM" in config.architectures[0]:
+        return AutoModelForCausalLM
+    elif config.architectures and "Seq2SeqLM" in config.architectures[0]:
+        return AutoModelForSeq2SeqLM
+    else:
+        return AutoModel
+# custom input depending on model task
+def get_dummy_input(model_name, config):
+    task = MODEL_TASKS.get(model_name, "text")
+
+    if task == "vision":
+        return {
+            "pixel_values": torch.randn(1, 3, 224, 224, device="cpu", dtype=torch.float32)
+        }
+    else:  
+        return {
+            "input_ids": torch.randint(0, 1000, (1, 16), device="cpu")
+        }
+
+@thunder._with_cache_info_ctx
+def run_prologue(jfn, *args, **kwargs):
+    cd = thunder.compile_data(jfn)
+    cs = thunder.compile_stats(jfn)
+    ci = thunder._get_cache_info()
+    cd.populate_cache_info(ci, *args, **kwargs)
+    prologue_trc, computation_trc, epilogue_trc = cd.acquire_initial_trace(
+        cd.fn, args, kwargs, cd, cs, cd.executors_list[0]
+    )
+    cache_entry = cd.apply_transforms_and_build_cache_entry(cd, cs, ci, prologue_trc, computation_trc, epilogue_trc)
+    with thunder.compile_data_and_stats(cd, cs):
+        pro_to_comp, pro_to_epi = cache_entry.prologue_fn(*args, **kwargs)
+    return cache_entry, pro_to_comp, pro_to_epi
+
+def try_model(model_name):
+    print(f"\n=== Testing {model_name} ===")
+
+    try:
+        config = AutoConfig.from_pretrained(model_name)
+        model_class = get_model_class(model_name, config)
+        model = model_class.from_config(config).to("meta")
+        input_sample = get_dummy_input(model_name, config)
+
+    except Exception as setup_err:
+        print(f"[SKIPPED] {model_name} - model setup failed")
+        traceback.print_exc()
+        return  
+    
+    try:
+        jmodel = thunder.jit(model)
+        ce, pro_to_comp, pro_to_epi = run_prologue(jmodel, **input_sample)
+        print(f"[SUCCESS] {model_name} Trace acquired!")
+
+    except Exception as thunder_err:
+        print(f"[FAILURE] {model_name} - Thunder trace acquisition failed")
+        traceback.print_exc()
+
+if __name__ == "__main__":
+    for model in MODEL_LIST:
+        try_model(model)
