b4632584f1fc619edc0ef85c8b384d9d87da16c2
Luca Antiga <luca@lightning.ai>
2025-06-30 15:00:01 +0200

    Add jit coverage CI job on HF models

---
+++ .github/run-jit-coverage-as-lit-studios.py
@@ -0,0 +1,42 @@
+import sys
+from datetime import datetime
+import glob
+
+from lightning_sdk import Studio, Machine
+
+
+def main(gh_run_id: str = ""):
+    if not gh_run_id:
+        gh_run_id = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
+
+    print("Creating studio...")
+    s = Studio( f"thunder-jit-coverage-run{gh_run_id}", "oss-thunder", org="lightning-ai", create_ok=True)
+    s.switch_machine(machine=Machine.L40S, interruptible=True)
+
+    print("Uploading package and scripts...")
+    s.upload_folder("dist", remote_path="dist")
+    pkg_path = glob.glob("dist/*.whl")[0]
+    s.upload_folder("examples/coverage", remote_path="coverage")
+
+    print("Starting studio...")
+    s.start()
+    print("Installing Thunder and other requirements...")
+    s.run(f"pip install {pkg_path} -U -r coverage/requirements.txt")
+
+    print("Running thunder.jit coverage...")
+    s.run("python coverage/jit_coverage_hf.py --models-file coverage/all.txt --output-dir data --results-file data.json")
+    
+    data_json = s.run("cat data.json")
+
+    with open("jit_coverage_report.json", "w") as f:
+        f.write(data_json)
+
+    print("Stopping studio...")
+    s.stop()
+    s.delete()
+
+
+if __name__ == "__main__":
+    # parse command line arguments
+    args = sys.argv[1:]
+    main(*args)
