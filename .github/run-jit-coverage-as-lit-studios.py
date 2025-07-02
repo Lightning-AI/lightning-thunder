be0bd51c9e9311e44e989cb1db3b88d4550586da
Luca Antiga <luca@lightning.ai>
2025-07-02 14:09:19 +0200

    Improve reporting

--- .github/run-jit-coverage-as-lit-studios.py
+++ .github/run-jit-coverage-as-lit-studios.py
@@ -12,15 +12,18 @@ def main(gh_run_id: str = ""):
         gh_run_id = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
 
     filenames = []
+    batches = []
+    n_models = 0
     chunk_size = 16
     with open("examples/coverage/all.txt") as f:
         lines = [el for el in f.readlines()]
+        n_models = len(lines)
         chunks = [lines[i : i + chunk_size] for i in range(0, len(lines), chunk_size)]
         for i, chunk_lines in enumerate(chunks):
             filename = f"{i:03d}.txt"
             with open(f"examples/coverage/{filename}", "w") as f:
                 f.writelines([el + "\n" for el in chunk_lines])
-            filenames.append(filename)
+            batches.append((filename, chunk_lines))
 
     print("Creating studio...")
     s = Studio(f"thunder-jit-coverage-run{gh_run_id}", "oss-thunder", org="lightning-ai", create_ok=True)
@@ -36,25 +39,38 @@ def main(gh_run_id: str = ""):
     print("Installing Thunder and other requirements...")
     s.run(f"pip install {pkg_path} -U -r coverage/requirements.txt")
 
-    with open("examples/coverage/all.txt") as f:
-        lines = [el for el in f.readlines()]
-
     hf_token = os.environ["HF_TOKEN"]
+    n_processed = 0
     print("Running thunder.jit coverage...")
-    for filename in filenames:
+    for filename, models in batches:
+        n_processed += len(models)
+        print(f"Processed {n_processed}/{n_models}")
+        print(f"Processing:"
+        for model in models:
+            print(f"  model")
         out = s.run(
             f"HF_TOKEN={hf_token} python coverage/jit_coverage_hf.py --models-file coverage/{filename} --output-dir data"
         )
-        print(f"Processing {filename}")
-        print(out)
+    print(f"Processed {n_processed}/{n_models}")
 
     print("Aggregating results...")
     s.run("python coverage/jit_coverage_hf.py --output-dir data --results-file data.json")
 
     data_json = s.run("cat data.json")
     data = json.loads(data_json)
-    for el in data:
-        print(el)
+    success = [el for el in data if el["status"] == "[SUCCESS]"]
+    skipped = [el for el in data if el["status"] == "[SKIPPED]"]
+    failure = [el for el in data if el["status"] == "[FAILURE]"]
+
+    for el in success:
+        print(f"🟢 [SUCCESS] {el['model']}")
+
+    for el in skipped:
+        print(f"🟡 [SKIPPED] {el['model']}")
+
+    for el in failure:
+        print(f"⛔️ [FAILURE] {el['model']}")
+        print(f"   Error:    {el['last']}")
 
     with open("jit_coverage_report.json", "w") as f:
         f.write(data_json)
