2f42fa2c0c8fc866aefad322ee1777d94214abe7
Masaki Kozuki <mkozuki@nvidia.com>
2025-07-18 18:06:12 +0900

    [no ci] add crossentropy
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- thunder/executors/cutlass_dsl_ex.py
+++ thunder/executors/cutlass_dsl_ex.py
@@ -13,6 +13,7 @@ from thunder.core.proxies import TensorProxy
 import thunder.torch as ltorch
 
 if TYPE_CHECKING:
+    from typing import Any
     from thunder.core.dtypes import dtype as thunder_dtype
 
 
@@ -76,8 +77,13 @@ cutlass_dsl_ex = OperatorExecutor("cutlass_dsl", version=cutlass_dsl_version())
 register_executor(cutlass_dsl_ex)
 
 
+def is_device_quack_compat() -> bool:
+    return torch.cuda.get_device_capability() in ((9, 0), (10, 0))
+
+
 # Register [`quack`](https://github.com/Dao-AILab/quack) ops
 if find_spec("quack") is not None:
+    # softmax
     from quack.softmax import _softmax_fwd, _softmax_backward
 
     def quack_softmax_impl(a: torch.Tensor) -> torch.Tensor:
@@ -113,7 +119,7 @@ if find_spec("quack") is not None:
     ) -> bool:
         last_dims = {-1, a.ndim - 1}
         allowed_dtypes = {None, a.dtype}
-        return dim in last_dims and dtype in allowed_dtypes and torch.cuda.get_device_capability() in ((9, 0), (10, 0))
+        return dim in last_dims and dtype in allowed_dtypes and is_device_quack_compat()
 
     def quack_softmax_transform(
         a: TensorProxy,
@@ -145,3 +151,125 @@ if find_spec("quack") is not None:
             execution_transform=quack_softmax_transform,
             grad_transform=quack_softmax_grad,
         )
+
+    # crossentropy
+    from quack.cross_entropy import _cross_entropy, _cross_entropy_backward
+
+    def quack_cross_entropy_forward_impl(
+        x: torch.Tensor,
+        target: torch.Tensor,
+    ) -> torch.Tensor:
+        return _cross_entropy(x, target, return_lse=False)
+
+    def quack_cross_entropy_forward_meta(x: TensorProxy, target: TensorProxy) -> TensorProxy:
+        return TensorProxy(like=x, shape=(x.shape[0],))
+
+    quack_cross_entropy_forward = cutlass_dsl_ex.register_operator(
+        "cutlass_quack_cross_entropy_forward",
+        meta=quack_cross_entropy_forward_meta,
+        fn=quack_cross_entropy_forward_impl,
+    )
+
+    def quack_cross_entropy_backward_impl(
+        x: torch.Tensor,
+        target: torch.Tensor,
+        grad: torch.Tensor,
+        lse: torch.Tensor,
+    ) -> torch.Tensor:
+        return _cross_entropy_backward(x, target, grad, lse, False)
+
+    def quack_cross_entropy_backward_meta(
+        x: TensorProxy,
+        target: TensorProxy,
+        grad: TensorProxy,
+        lse: TensorProxy,
+    ) -> TensorProxy:
+        return TensorProxy(like=grad)
+
+    quack_cross_entropy_backward = cutlass_dsl_ex.register_operator(
+        "cutlass_quack_cross_entropy_backward",
+        meta=quack_softmax_backward_meta,
+        fn=quack_cross_entropy_backward_impl,
+    )
+
+    def quack_cross_entropy_checker(
+        a: TensorProxy,
+        /,
+        target: TensorProxy,
+        weight: TensorProxy | None = None,
+        size_average: bool | None = None,
+        ignore_index: int = -100,
+        reduce: bool | None = None,
+        reduction: str = "mean",
+        label_smoothing: float = 0.0,
+    ) -> bool:
+        if not is_device_quack_compat():
+            return False
+        if weight is not None:
+            return False
+
+        # Assert deprecated flags are not used
+        for boolean_flag in (size_average, reduce):
+            if boolean_flag is not None:
+                return False
+
+        if reduction != "none":
+            return False
+
+        if label_smoothing != 0.0:
+            return False
+
+        return True
+
+    def quack_cross_entropy_transform(
+        a: TensorProxy,
+        /,
+        target: TensorProxy,
+        weight: TensorProxy | None = None,
+        size_average: bool | None = None,
+        ignore_index: int = -100,
+        reduce: bool | None = None,
+        reduction: str = "mean",
+        label_smoothing: float = 0.0,
+    ) -> TensorProxy:
+        return quack_cross_entropy_forward(a, target)
+
+    def quack_cross_entropy_aug_forward_impl(
+        x: torch.Tensor,
+        target: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        return _cross_entropy(x, target, return_lse=True)
+
+    def quack_cross_entropy_aug_forward_meta(a: TensorProxy, target: TensorProxy) -> tuple[TensorProxy, TensorProxy]:
+        return (TensorProxy(like=a, shape=(a.shape[0],)), TensorProxy(like=a, shape=(a.shape[0],)))
+
+    quack_cross_entropy_aug_forward = cutlass_dsl_ex.register_operator(
+        "cutlass_quack_cross_entropy_aug_forward",
+        meta=quack_cross_entropy_aug_forward_meta,
+        fn=quack_cross_entropy_aug_forward_impl,
+    )
+
+    def quack_cross_entropy_grad(
+        a: TensorProxy,
+        /,
+        target: TensorProxy,
+        weight: TensorProxy | None = None,
+        size_average: bool | None = None,
+        ignore_index: int = -100,
+        reduce: bool | None = None,
+        reduction: str = "mean",
+        label_smoothing: float = 0.0,
+    ) -> TensorProxy:
+        fwd, lse = quack_cross_entropy_aug_forward(a, target)
+        g = get_grad(fwd)
+        a_grad = quack_cross_entropy_backward(a, target, g, lse)
+        put_grad(a, a_grad)
+
+        return fwd
+
+    cutlass_dsl_ex.register_implementation(
+        ltorch.cross_entropy,
+        checker=quack_cross_entropy_checker,
+        execution_transform=quack_cross_entropy_transform,
+        grad_transform=quack_cross_entropy_grad,
+    )
