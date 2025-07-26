bbe354a8d43a58119a693c8f59578c902326fc2d
Masaki Kozuki <mkozuki@nvidia.com>
2025-07-26 18:36:38 +0900

    Implement `thunder.torch.register_custom_op`
    with which we can register torch.compile compliant custom operators to
    thunder and even we could give it our own custom implementations written
    in such as nvfuser and cudnn-frontend.
    
    With this commit, `thunder.jit(torch.ops.my_lib.f, executors=[custom_op_ex])` works!
    
    Obtained `thunder.last_traces(jitted)[-1]`:
    ```python
    def computation(t_0, t_1):
      # t_0: "cuda:0 f32[4, 2]"
      # t_1: "cuda:0 f32[4, 2]"
      t2 = my_custom_op_mul(t_0, t_1)  # t2: "cuda:0 f32[4, 2]"
      return (t2,)
    ```
    
    Script:
    ```python
    from __future__ import annotations
    
    import numpy as np
    import torch
    from torch._library.custom_ops import CustomOpDef
    
    import thunder
    from thunder.torch import register_custom_op
    from thunder.executors.custom_op_ex import custom_op_ex
    
    @torch.library.custom_op("my_custom_op::mul", mutates_args=())
    def mul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
        return a * b
    
    @torch.library.register_kernel("my_custom_op::mul", "cpu")
    def _(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
        return torch.from_numpy(np.multiply(
            a.numpy(force=True),
            b.numpy(force=True),
        ))
    
    @torch.library.register_kernel("my_custom_op::mul", "cuda")
    def _(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
        return a * b
    
    @torch.library.register_fake("my_custom_op::mul")
    def _(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
        return torch.empty_like(a)
    
    if __name__ == "__main__":
        register_custom_op(mul)
        with torch.device("cuda"):
            x = torch.randn((4, 2))
            y = torch.randn((4, 2))
    
        jitted = thunder.jit(torch.ops.my_custom_op.mul, executors=[custom_op_ex])
        out = jitted(x, y)
        print(thunder.last_traces(jitted)[-1])
    ```
    
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

---
+++ thunder/torch/custom_ops.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from thunder.core.symbol import Symbol
+from thunder.core.prims import OpTags
+
+if TYPE_CHECKING:
+    from torch._library.custom_ops import CustomOpDef
+    from torch._ops import OpOverload
+    from torch._ops import OpOverloadPacket
+
+
+__all__ = [
+    "register_custom_op",
+]
+
+
+# TODO: Interpret backward
+# TODO: Figure out if we can access `triton_op` of the same name
+def register_custom_op(custom_op: CustomOpDef) -> Symbol:
+    """Register :func:`~torch.library.custom_op`'ed function to Thunder.
+
+    Args:
+        custom_op: :func:`torch.library.custom_op`'ed function. This is not ``torch.ops.{namespace}.{name}``.
+
+    Returns:
+        :class:`~thunder.core.symbol.Symbol`: A symbol representing the input ``custom_op``.
+    """
+    from thunder.core.langctxs import langctx, Languages
+    from thunder.executors.torchex import _always_executable
+    from thunder.torch import meta_adaptor, _torch_to_thunder_function_map
+    from thunder.executors.custom_op_ex import custom_op_ex
+
+    # `custom_op` is `custom_op(name)(my_func)`,
+    # `torch.ops.namespace.name` is `OpOverloadPacket.`
+    # e.g. `torch.ops.my_lib.foo` is OpOverloadPacket and `torch.ops.my_lib.foo.default` is OpOverload.
+    torch_opoverload: OpOverload = custom_op._opoverload
+    torch_opoverload_packet: OpOverloadPacket = torch_opoverload._overloadpacket
+
+    meta_fn = langctx(Languages.TORCH)(meta_adaptor(custom_op._abstract_fn))
+    fn_name = custom_op._qualname.replace("::", "_")
+    op_id = f"torch::ops::{custom_op._qualname}".replace("::", ".")
+    symbol = Symbol(
+        name=fn_name,
+        meta=meta_fn,
+        id=op_id,
+        is_prim=False,
+        tags=(OpTags.AUTO_REGISTERED,),
+    )
+    op = custom_op_ex.register_operator(fn_name, meta=meta_fn, fn=torch_opoverload)
+    custom_op_ex.register_implementation(symbol, op, checker=_always_executable)
+    _torch_to_thunder_function_map[torch_opoverload_packet] = symbol
+    return symbol
