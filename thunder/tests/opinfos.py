03bfda7334bee0777f02e97e29dc0c1fc1b40942
shino16 <40788005+shino16@users.noreply.github.com>
2025-07-29 16:11:30 +0000

    Specify torch.randint's default dtype (#2342)

--- thunder/tests/opinfos.py
+++ thunder/tests/opinfos.py
@@ -6835,7 +6835,7 @@ randint_opinfo = OpInfo(
     sample_input_generator=varargs_tensor_creation_op_sample_generator_with_bounds,
     error_input_generator=randn_error_generator,  # Does not depend on the distribution
     torch_reference=lambda *args, **kwargs: torch.randint(*args, **kwargs).fill_(0),
-    dtypes=(datatypes.int64,),
+    dtypes=(datatypes.int64, datatypes.floating),
 )
 tensor_creation_ops.append(randint_opinfo)
 
--- thunder/tests/test_core.py
+++ thunder/tests/test_core.py
@@ -2863,6 +2863,14 @@ def test_arange_default_dtype():
     assert jfn() == torch.int64
 
 
+def test_randint_default_dtype():
+    def fn():
+        return torch.randint(0, 5, (2, 3))
+
+    jfn = thunder.jit(fn)
+    assert jfn().dtype == fn().dtype == torch.int64
+
+
 def test_cat_mixed_dtypes():
     # We add a special test here instead of a sample in OpInfo.
     # When we add a mixed input sample in OpInfo, it will also be picked up for the test which
