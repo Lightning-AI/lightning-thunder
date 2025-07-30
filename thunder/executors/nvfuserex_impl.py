Atempt to fix the precision error

thunder/executors/nvfuserex_impl.py
@@ -76,9 +76,6 @@ from thunder.executors.nvfuserex import nvfuser_version
 import nvfuser
 from nvfuser import DataType, FusionDefinition
 
-nvTensor = nvfuser._C.Tensor
-nvNumber = nvfuser._C.Scalar
-
 #
 # Helper functions
 #
@@ -3196,10 +3193,9 @@ def cumsum_transform(
     a: TensorProxy, dim: int, /, dtype: torch.dtype | None = None, *, fd: FusionDefinition, lc_to_nv_map: dict
 ) -> TensorProxy:
     # Emulate cumsum using matmul: cumsum(a) = a @ triu(ones)
-    if dtypes.is_integer_dtype(a.dtype) or dtypes.is_low_precision_dtype(a.dtype):
+    if dtypes.is_integer_dtype(a.dtype):
         # torch.matmul can't do integers on GPU so we convert `a` to
-        # float. Low precision matmuls have large numerical errors so we
-        # convert `a` to float as well.
+        # float.
         compute_dtype = DataType.Float
     else:
         compute_dtype = lcdtype_to_nvdtype(a.dtype)

thunder/tests/test_ops.py
@@ -78,13 +78,23 @@ def snippet_torch_consistency(op: OpInfo, torch_op, sample: SampleInput, comp: C
         comp(thunder_result, torch_result)
     except AssertionError:
 
-        def upcast_tensors(x):
+        def is_floating_point_dtype(dtype: torch.dtype) -> bool:
+            return torch.tensor([], dtype=dtype).is_floating_point()
+
+        def upcast(x):
             if isinstance(x, torch.Tensor) and torch.is_floating_point(x):
                 return x.to(torch.double)
+
+            # Some torch APIs (e.g. cumsum) take a `dtype` argument and use it
+            # as the **compute** dtype. Replace that with torch.double so the
+            # reference result is more likely to be computed in double.
+            if isinstance(x, torch.dtype) and is_floating_point_dtype(x):
+                return torch.double
+
             return x
 
-        reference_args = tree_map(upcast_tensors, args)
-        reference_kwargs = tree_map(upcast_tensors, kwargs)
+        reference_args = tree_map(upcast, args)
+        reference_kwargs = tree_map(upcast, kwargs)
         reference_result = torch_op(*reference_args, **reference_kwargs)
 
         assert_closer(