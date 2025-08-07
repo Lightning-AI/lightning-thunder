13d7c335342d6a2059037d807851f4aa15cff866
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2025-08-07 18:09:06 +0300

    move global state reset to the start of the test

--- thunder/tests/test_transformer_engine_v2_executor.py
+++ thunder/tests/test_transformer_engine_v2_executor.py
@@ -518,6 +518,11 @@ def test_te_activation_checkpointing_correctness(fp8_recipe: recipe.Recipe, comp
     device = "cuda"
     iterations = 6
 
+    from transformer_engine.pytorch.fp8 import FP8GlobalStateManager
+
+    # Before starting, reset the state manager.
+    FP8GlobalStateManager.reset()
+
     checkpoint_fn = partial(torch.utils.checkpoint.checkpoint, use_reentrant=False)
 
     input_shape = (768, 4096)
@@ -555,8 +560,6 @@ def test_te_activation_checkpointing_correctness(fp8_recipe: recipe.Recipe, comp
 
     # TE does not expose the scales for MXFP8
     if fp8_recipe.delayed():
-        from transformer_engine.pytorch.fp8 import FP8GlobalStateManager
-
         te_scales = []
         te_amax_hist = []
         with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe):
@@ -642,6 +645,3 @@ def test_te_activation_checkpointing_correctness(fp8_recipe: recipe.Recipe, comp
         # check that amax history is the same as TE
         for te_amax, th_amax in zip(te_amax_hist, th_amax_hist):
             assert_close(te_amax[:, :-1], th_amax)
-
-        # Reset state manager before next run.
-        FP8GlobalStateManager.reset()
