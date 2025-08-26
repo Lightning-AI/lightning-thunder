8fb6aabd1fede19efd25d207cdeb3fe7aa3b6c60
beverlylytle <blytle@nvidia.com>
2025-08-26 13:05:21 +0300

    fix pytest fixture

--- thunder/tests/test_update_aliases.py
+++ thunder/tests/test_update_aliases.py
@@ -86,6 +86,9 @@ for op in opinfos:
 def turn_off_tf32_and_set_seed(monkeypatch):
     monkeypatch.setenv("NVIDIA_TF32_OVERRIDE", "0")
     torch.manual_seed(42)
+    yield
+    monkeypatch.delenv("NVIDIA_TF32_OVERRIDE")
+    torch.seed()
 
 
 @instantiate(
