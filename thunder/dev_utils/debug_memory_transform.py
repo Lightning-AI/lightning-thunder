30b254b6845f24caa84852eede68e7c5bcaf36a1
Masato Shinokawa <shino16@users.noreply.github.com>
2025-09-18 02:16:31 -0700

    Support device_index

--- thunder/dev_utils/debug_memory_transform.py
+++ thunder/dev_utils/debug_memory_transform.py
@@ -4,17 +4,18 @@ from thunder.core.prims import PrimIDs
 
 
 class DebugMemoryTransform(DebugTransform):
-    def __init__(self):
+    def __init__(self, device_index=0):
+        self.device_index = device_index
         self.memory_events = []
 
         def collect_memory_events(bsym, output, *args, **kwargs):
             torch.cuda.synchronize()
-            snapshot = torch.cuda.memory._snapshot()
-            events, = snapshot["device_traces"]
+            snapshot = torch.cuda.memory._snapshot(device=self.device_index)
+            events = snapshot["device_traces"][self.device_index]
             new_events = [event for event in events if event["size"] > 4]
-            alloc = torch.cuda.memory_allocated()
+            alloc = torch.cuda.memory_allocated(device=self.device_index)
             self.memory_events[-1].append((bsym, alloc, new_events))
-            torch.cuda.memory._record_memory_history(clear_history=True)
+            torch.cuda.memory._record_memory_history(clear_history=True, device=self.device_index)
 
             return "\n".join(f"{event["action"]} - {event["size"]} bytes" for event in new_events) + f"\ntotal {alloc} bytes allocated"
 
@@ -31,33 +32,34 @@ class DebugMemoryTransform(DebugTransform):
         return debug_bsym
 
     def transform_trace_post_optimization(self, trace, **kwargs):
-        torch.cuda.memory._record_memory_history(clear_history=True)
+        torch.cuda.memory._record_memory_history(clear_history=True, device=self.device_index)
         self.memory_events.append([])
         return super().transform_trace_post_optimization(trace, **kwargs)
 
 
 class DebugMemoryFXTransform:
-    def __init__(self):
+    def __init__(self, device_index=0):
+        self.device_index = device_index
         self.memory_events = []
         self.augumented_graph_modules = []
 
     def _collect_memory_events(self, node: torch.fx.Node):
         def inner_fn():
             torch.cuda.synchronize()
-            snapshot = torch.cuda.memory._snapshot()
-            events, = snapshot["device_traces"]
+            snapshot = torch.cuda.memory._snapshot(device=self.device_index)
+            events = snapshot["device_traces"][self.device_index]
             new_events = [event for event in events if event["size"] > 4]
-            alloc = torch.cuda.memory_allocated()
+            alloc = torch.cuda.memory_allocated(device=self.device_index)
             self.memory_events[-1].append((node, alloc, new_events))
             node.next.stack_trace = f"File \"<debug>\", line 0, in <debug>\nmemory_events = [" + ", ".join(f"{event["action"]} - {event["size"]} bytes" for event in new_events) + f"], total {alloc} bytes allocated"
-            torch.cuda.memory._record_memory_history(clear_history=True)
+            torch.cuda.memory._record_memory_history(clear_history=True, device=self.device_index)
 
         inner_fn.__name__ = f"memory_events_{node.name}"
 
         return inner_fn
 
     def __call__(self, graph_module: torch.fx.GraphModule):
-        torch.cuda.memory._record_memory_history(clear_history=True)
+        torch.cuda.memory._record_memory_history(clear_history=True, device=self.device_index)
         self.memory_events.append([])
         nodes = list(graph_module.graph.nodes)
         for node in nodes:
