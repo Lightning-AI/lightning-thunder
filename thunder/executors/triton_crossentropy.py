7f806609cac0263bdfb0c84ff95df76207ef05f7
Luca Antiga <luca.antiga@gmail.com>
2025-09-24 08:56:53 +0000

    Autodetect CPU environment from compile entrypoint

--- thunder/executors/triton_crossentropy.py
+++ thunder/executors/triton_crossentropy.py
@@ -1,16 +1,20 @@
 from thunder.executors import triton_utils
 from thunder.extend import OperatorExecutor
 
+import torch
+
 triton_version: None | str = triton_utils.triton_version()
 
 triton_ex: None | OperatorExecutor = None
-if triton_version is not None:
-    try:
-        from thunder.executors.triton_crossentropy_impl import triton_ex as impl_ex
 
-        triton_ex = impl_ex
-    except Exception:
-        import warnings
+if torch.cuda.is_available():
+    if triton_version is not None:
+        try:
+            from thunder.executors.triton_crossentropy_impl import triton_ex as impl_ex
 
-        warnings.warn("triton is present but cannot be initialized")
-        triton_version = None
+            triton_ex = impl_ex
+        except Exception:
+            import warnings
+
+            warnings.warn("triton is present but cannot be initialized")
+            triton_version = None
