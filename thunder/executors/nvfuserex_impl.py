8b6a38da19a8739781a9efca9718bd5ba9fbecc7
Masaki Kozuki <mkozuki@nvidia.com>
2025-09-26 03:28:04 +0900

    fp4 since 0.2.28
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- thunder/executors/nvfuserex_impl.py
+++ thunder/executors/nvfuserex_impl.py
@@ -119,6 +119,12 @@ if nvfuser_version() >= LooseVersion("0.2.27"):
         {
             dtypes.uint64: DataType.UInt64,
             dtypes.uint64_: DataType.UInt64,
+        }
+    )
+
+if nvfuser_version() >= LooseVersion("0.2.28"):
+    _lcdtype_to_nvdtype_map.update(
+        {
             dtypes.float4_e2m1fn_x2: DataType.Float4_e2m1fn_x2,
             dtypes.float4_e2m1fn_x2_: DataType.Float4_e2m1fn_x2,
         }
