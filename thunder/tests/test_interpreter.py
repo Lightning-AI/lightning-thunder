c8451ad8f774bee89ad71a1315b17164d6dc6bde
Masaki Kozuki <mkozuki@nvidia.com>
2025-10-14 16:48:48 +0900

    Enable E722
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- thunder/tests/test_interpreter.py
+++ thunder/tests/test_interpreter.py
@@ -620,7 +620,7 @@ def test_bare_except(jit):
         try:
             assert is_jitting_with_raise() == jitting
             raise ValueError(msg)
-        except:
+        except Exception:
             assert is_jitting_with_raise() == jitting
             return True
 
@@ -839,7 +839,7 @@ def test_uncaught_exception_no_leak():
 
         try:
             model(x)
-        except:
+        except Exception:
             pass
         return weakref.ref(x)
 
