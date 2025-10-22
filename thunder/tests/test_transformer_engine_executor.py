1f1965a04bb08461c67fdbc1ba89f6679cd4b91f
Thomas Viehmann <tv.code@beamnet.de>
2025-10-21 18:43:10 -0700

    CUDA

--- thunder/tests/test_transformer_engine_executor.py
+++ thunder/tests/test_transformer_engine_executor.py
@@ -717,7 +717,7 @@ def test_te_activation_checkpointing_correctness(fp8_recipe: recipe.Recipe, comp
             te_assert_close(te_amax[:, :-1], th_amax)
 
 
-@requirescuda
+@requiresCUDA
 @pytest.mark.skipif(LooseVersion(transformer_engine.__version__) < LooseVersion("2.9"))
 def test_te_inference_8bit():
     from thunder.transforms.te_inference import TEInference8BitTransform
