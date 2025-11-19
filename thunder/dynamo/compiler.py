b5da2a552f588bf9b6a93008dc17534e07807346
Kaixi Matteo Chen <kaiximatteoc@nvidia.com>
2025-11-19 17:29:53 +0100

    Supported kwargs in ThunderCompiler

--- thunder/dynamo/compiler.py
+++ thunder/dynamo/compiler.py
@@ -129,7 +129,7 @@ class ThunderCompiler:
         )
         self.thunder_options = thunder_options
 
-    def __call__(self, gm: torch.fx.GraphModule, sample_args: list[torch.SymInt, torch.Tensor]):
+    def __call__(self, gm: torch.fx.GraphModule, sample_args: list[torch.SymInt, torch.Tensor], **kwargs):
         from thunder import jit
 
         remove_empty_autocast(gm)
@@ -148,6 +148,7 @@ class ThunderCompiler:
             gm,
             partial(jit, **thunder_options),
             thunder_options,
+            **kwargs,
         )
         self.subgraph_infos.append(subgraph_info)
         return split_module
