bc5e5321fcd4cd3a0fdc718891089d45b30ad014
riccardofelluga <11768013+riccardofelluga@users.noreply.github.com>
2025-11-20 18:12:45 +0200

    update comment and add None guard for transform

--- thunder/plugins/fp8.py
+++ thunder/plugins/fp8.py
@@ -5,7 +5,7 @@ class FP8(Plugin):
     """
     Plugin for enabling FP8 precision via NVIDIA Transformer Engine, enabling higher throughput of matrix operations in FP8.
 
-    See `lightning-thunder/thunder/executors/transformer_engine_ex.py` for implementation details.
+    See `lightning-thunder/thunder/executors/transformer_engineex.py` for implementation details.
     """
 
     def setup_transforms(self):
@@ -17,6 +17,10 @@ class FP8(Plugin):
         """
 
         from thunder.executors.transformer_engineex import TransformerEngineTransform
+
+        # When TE executor is not available, both the transform and the executor will be None.
+        if TransformerEngineTransform is None:
+            return []
 
         return [TransformerEngineTransform()]
 
